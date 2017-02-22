bea84cf747f529d142fb38a5af9f50dee89114c1
Aaron Andrews <aaronandrews.home@gmail.com>
2017-02-22 13:35:16 +1100

    neo4j api testing

--- services/topology-engine/app/api/__init__.py
+++ services/topology-engine/app/api/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import requests
+import json
 from flask import Flask, render_template
 
 
@@ -11,8 +12,13 @@ def index():
     try:
         headers = {'Content-Typet': 'application/json'}
         data = {'query' : 'MATCH (n) return n'}
-        r = requests.post('http://neo4j:7474/db/data/cypher', data=data, headers=headers, auth=('neo4j', 'temppass'))
-        return r.text
+        result = requests.post('http://neo4j:7474/db/data/cypher', data=data, headers=headers, auth=('neo4j', 'temppass'))
+        j = json.loads(result.text)
+        for n in j['data']:
+            n
+            for r in n:
+                r['outgoing_typed_relationships']
+        return str(j)
     except Exception as e:
         return "error: {}".format(str(e))
 
