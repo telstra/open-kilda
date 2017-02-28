8de6758dca0aba4a999fdd6a1787a4101dbe2df9
Aaron Andrews <aaronandrews.home@gmail.com>
2017-02-28 17:13:13 +1100

    api updates and threading fixes

--- services/topology-engine/app/api/__init__.py
+++ services/topology-engine/app/api/__init__.py
@@ -12,6 +12,7 @@ def index():
     try:
         headers = {'Content-Typet': 'application/json'}
         data = {'query' : 'MATCH (n) return n'}
+
         result_switches = requests.post('http://neo4j:7474/db/data/cypher', data=data, headers=headers, auth=('neo4j', 'temppass'))
         j_switches = json.loads(result_switches.text)
         topology = []
@@ -21,13 +22,13 @@ def index():
                 node['name'] = (r['data']['name'])
                 result_relationships = requests.get(str(r['outgoing_relationships']), auth=('neo4j', 'temppass'))
                 j_paths = json.loads(result_relationships.text)
+                outgoing_relationships = []
                 for j_path in j_paths:
-                    outgoing_relationships = []
                     if j_path['type'] == u'isl':
                         outgoing_relationships.append(j_path['data']['dst_switch'])
-                    node['outgoing_relationship'] = outgoing_relationships
+                    node['outgoing_relationships'] = outgoing_relationships
             topology.append(node)
-        return json.dumps(topology)
+        return str(json.dumps(topology, default=lambda o: o.__dict__, sort_keys=True, indent=4))
     except Exception as e:
         return "error: {}".format(str(e))
 
--- services/topology-engine/app/queue-engine/topologylistener/functions.py
+++ services/topology-engine/app/queue-engine/topologylistener/functions.py
@@ -21,47 +21,41 @@ def get_event():
 
 def topo_event_handler(event):
     eventHandled = False
+    handleCount = 0
     while not eventHandled:
         if event.data['message_type'] == "switch" and event.data['state'] == "ADDED":
             eventHandled = create_switch(event.data['switch_id'])
         if event.data['message_type'] == "isl":
             eventHandled = create_isl(event.data['path'])
         if not eventHandled:
-            time.sleep(1)
-    print "{} Event processed".format("{:%d %b, %Y %H:%M:%S}".format(datetime.now()))
+            handleCount += 1
+            if handleCount > 50:
+                time.sleep(.1)
+    print "{} Event processed for: {}".format("{:%d %b, %Y %H:%M:%S}".format(datetime.now()), event.data['message_type'])
 
 def create_switch(switchid):
-    
-    print "Creating switch '{0}'.".format(switchid)
     switchExists = bool(db.runner("MATCH (a:switch{{ name: '{0}' }}) return count(a)".format(switchid)).single()[0])
-    
     if not switchExists:
         query = "CREATE (:switch {{ name: '{0}' }})".format(switchid)
         db.runner(query)
-        print "Event: switch added to topology"
         return True
     else:
         print "Switch '{0}' already exists in topology.".format(switchid)
         return True
 
 def create_isl(path):
-    
     a_switch = path[0]['switch_id']
     a_port = path[0]['port_no']
     b_switch = path[1]['switch_id']
     b_port = path[1]['port_no']
     a_switchExists = bool(db.runner("MATCH (a:switch{{ name: '{0}' }}) return count(a)".format(a_switch)).single()[0])
     b_switchExists = bool(db.runner("MATCH (a:switch{{ name: '{0}' }}) return count(a)".format(b_switch)).single()[0])
-    
     if not a_switchExists or not b_switchExists:
         return False
-    
     islExists = bool(db.runner("MATCH p=()-[r:isl{{ src_switch: '{0}',src_port: '{1}', dst_switch: '{2}', dst_port: '{3}'  }}]->() RETURN count(p)".format(a_switch, a_port, b_switch, b_port)).single()[0])
-    
     if not islExists:
         query = "MATCH (a:switch{{ name: '{0}' }}),(b:switch{{ name: '{2}' }}) CREATE(a) -[r:isl {{ src_switch: '{0}',src_port: '{1}', dst_switch: '{2}', dst_port: '{3}'  }}]-> (b)".format(a_switch, a_port, b_switch, b_port)
         db.runner(query)
-        print "Event: isl added to topology"
         return True
     else:
         print "ISL {0}:{1} -> {2}:{3} already exists.".format(a_switch, a_port, b_switch, b_port)
