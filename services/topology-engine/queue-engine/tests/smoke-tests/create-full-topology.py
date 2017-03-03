847215477ca3fda4ec32a4475ed19cd51b1aefa1
Carmine Rimi <carmine.rimi@gmail.com>
2017-03-03 14:10:38 +1100

    Fix bug in TE/QE smoke-test - create-full-topology
    The bug was that we needed to use json= instead of data=
    in the requests.post call.

--- services/topology-engine/queue-engine/tests/smoke-tests/create-full-topology.py
+++ services/topology-engine/queue-engine/tests/smoke-tests/create-full-topology.py
@@ -2,12 +2,14 @@
 from kafka import KafkaConsumer, KafkaProducer
 import requests
 import json
+import pprint
+
+headers = {'Content-Type': 'application/json'}
 
 def cleanup():
     print "Clearing exiting topology."
-    headers = {'Content-Type': 'application/json'}
-    result_clear = requests.post('http://localhost:38080/cleanup', headers=headers)
-    print "Successful"
+    result = requests.post('http://localhost:38080/cleanup', headers=headers)
+    print "Successful", result
 
 
 def create_topo():
@@ -15,9 +17,13 @@ def create_topo():
     with open('full-topology.json') as infile:
         j_data = json.load(infile)
 
-    result_switches = requests.post('http://localhost:38080/topology', data=j_data, headers=headers)
-    print result_switches.text
-    print "Successful"
+    result = requests.post('http://localhost:38080/topology', json=j_data, headers=headers)
+    if result.status_code == 200:
+        print "Successful"
+        print
+    else:
+        print "Failure:", result.status_code
+        print result.text
 
 cleanup()
 create_topo()
