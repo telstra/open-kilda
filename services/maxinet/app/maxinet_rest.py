f8279c45aca42095f939ba7f71937a5983a2c45e
david_donn <david.donn@team.telstra.com>
2017-03-07 09:23:51 +1100

    Completed server-side implementation of dynamicTopo test

--- services/maxinet/app/maxinet_rest.py
+++ services/maxinet/app/maxinet_rest.py
@@ -95,6 +95,73 @@ experiment_schema = {
   ]
 }
 
+host_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+      "name": {
+        "type": "string"
+      },
+      "pos": {
+        "type": "string"
+      },
+  },
+  "required": [
+    "name",
+    "pos"
+  ]
+}
+
+switch_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+      "name": {
+        "type": "string"
+      },
+      "workerId": {
+        "type": "integer"
+      }
+  },
+  "required": [
+    "name",
+    "workerId"
+  ]
+}
+
+link_schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+      "node1": {
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "type": {
+            "type": "string"
+          },
+        },
+      },
+      "node2": {
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "type": {
+            "type": "string"
+          },
+        },
+      }
+  },
+  "required": [
+    "node1",
+    "node2"
+  ]
+}
+
 @error(500)
 def error_handler_500(error):
     return json.dumps({"message": str(error.exception)})
@@ -188,6 +255,77 @@ def stop_experiment(name):
     response.content_type = 'application/json'
     return json.dumps({'name': name})
 
+@get('/experiment/run/<name>/<node>/<command>')
+def setup_experiment(name, node, command):
+    logger.debug("run command experiment={}, node={}, command={}" .format(name, node, command))
+    experiment = experiments[name]
+    output = experiment.get_node(node).cmd(command)
+    response.content_type = 'application/json'
+    return json.dumps({'name': name, 'node': node, 'command': command, 'output': output})
+
+@post('/experiment/<experiment>/host')
+def create_host(experiment):
+    try:
+        validate(request.json, host_schema)
+        host = _create_host(experiment, request.json)
+        response.content_type = 'application/json'
+        response.status = 201
+        response.set_header('Location', "http://experiment/{}/host/{}".format(experiment, host.name))
+        return json.dumps({'name': host.name, 'type': 'HOST'})
+    except ValidationError as e:
+        abort(400, e.message)
+
+def _create_host(experiment, host):
+    logger.debug("creating host for experiment={}, host={}, pos={}"
+                 .format(experiment, host['name'], host['pos']))
+    exp = experiments[experiment]
+    host_id = len(exp.hosts) + 1
+    hst = exp.addHost(host['name'], ip=Tools.makeIP(host_id), max=Tools.makeMAC(host_id), pos=host['pos'])
+    return hst
+
+@post('/experiment/<experiment>/switch')
+def create_switch(experiment):
+    try:
+        validate(request.json, switch_schema)
+        switch = _create_switch(experiment, request.json)
+        response.content_type = 'application/json'
+        response.status = 201
+        response.set_header('Location', "http://experiment/{}/switch/{}".format(experiment, switch.name))
+        return json.dumps({'name': switch.name, 'type': 'SWITCH'})
+    except ValidationError as e:
+        abort(400, e.message)
+
+def _create_switch(experiment, switch):
+    exp = experiments[experiment]
+    switch_id = len(exp.switches) + 1
+
+    logger.debug("creating switch for experiment={}, switch={}, workerId={}, dpid={}"
+                 .format(experiment, switch['name'], switch['workerId'], switch_id))
+    sw = exp.addSwitch(switch['name'], dpid=Tools.makeDPID(switch_id), wid=switch['workerId'])
+    return sw
+
+@post('/experiment/<experiment>/link')
+def create_link(experiment):
+    try:
+        validate(request.json, link_schema)
+        link = _create_link(experiment, request.json['node1'], request.json['node2'])
+        response.content_type = 'application/json'
+        response.status = 201
+
+        # TODO - how to get info from the returned "link" object?
+        response.set_header('Location', "http://experiment/{}/link/{}/{}".format(experiment, request.json['node1']['name'], request.json['node2']['name']))
+        return json.dumps({'node1': {'name': request.json['node1']['name'], 'type': request.json['node1']['type']}, 'node2': {'name': request.json['node2']['name'], 'type': request.json['node2']['type']}})
+    except ValidationError as e:
+        abort(400, e.message)
+
+def _create_link(experiment, node1, node2):
+    exp = experiments[experiment]
+
+    logger.debug("creating link for experiment={}, node1={}, node1type={}, node2={}, node2type={}"
+                 .format(experiment, node1['name'], node1['type'], node2['name'], node2['type']))
+    link = exp.addLink(node1['name'], node2['name'], autoconf=True)
+    return link
+
 def start_server(interface, port):
     run(host='0.0.0.0', port=port, debug=True)
 
@@ -198,6 +336,7 @@ def main():
     global experiments
     clusters = {}
     experiments = {}
+
     logging.basicConfig(format='%(levelname)s:%(message)s',
                         level=logging.DEBUG)
     logger = logging.getLogger()
