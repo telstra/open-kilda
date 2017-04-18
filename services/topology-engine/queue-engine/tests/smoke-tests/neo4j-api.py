da7c165de18b9088eb45d96ae2b6e16daa845da9
Aaron Andrews <aaronandrews.home@gmail.com>
2017-04-18 13:25:05 +1000

    updated smoke test

--- services/topology-engine/queue-engine/tests/smoke-tests/neo4j-api.py
+++ services/topology-engine/queue-engine/tests/smoke-tests/neo4j-api.py
@@ -5,7 +5,7 @@ import pprint
 
 url = "http://localhost/api/v1/flow"
 headers = {'Content-Type': 'application/json'}
-j_data = {"src_switch":"00:00:00:00:00:00:00:01", "src_port":1, "src_vlan":0, "dst_switch":"00:00:00:00:00:00:00:03", "dst_port":1, "dst_vlan":0}
+j_data = {"src_switch":"00:00:00:00:00:00:00:07", "src_port":1, "src_vlan":0, "dst_switch":"00:00:00:00:00:00:00:04", "dst_port":1, "dst_vlan":0}
 result = requests.post(url, json=j_data, headers=headers)
 print result.text
 
