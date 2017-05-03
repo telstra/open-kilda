4384d30805479a035eb01d6d5a4424337d2157a5
Vitaly Nazaryants <vinazaryants@mirantis.com>
2017-05-03 17:05:31 +0400

    Statistics requests

---
+++ src/floodlight-modules/src/main/java/org/bitbucket/openkilda/floodlight/message/command/StatsRequest.java
@@ -0,0 +1,54 @@
+package org.bitbucket.openkilda.floodlight.message.command;
+
+import com.fasterxml.jackson.annotation.*;
+import org.bitbucket.openkilda.floodlight.message.StatsType;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "command",
+        "destination",
+        "switch_id"
+})
+public class StatsRequest extends CommandData {
+
+    /** The switch id to request stats from. It is a mandatory parameter. */
+    protected String switchId;
+
+    private StatsType statsType;
+
+    public StatsRequest() {}
+
+    @JsonCreator
+    public StatsRequest(@JsonProperty("switch_id") String switchId, @JsonProperty("type") StatsType statsType) {
+        setSwitchId(switchId);
+        setStatsType(statsType);
+    }
+
+    @JsonProperty("switch_id")
+    public String getSwitchId() {
+        return switchId;
+    }
+
+    @JsonProperty("switch_id")
+    public void setSwitchId(String switchId) {
+        if (switchId == null) {
+            throw new IllegalArgumentException("need to set a switch_id");
+        } else if (!Utils.checkSwitchId(switchId)) {
+            throw new IllegalArgumentException("need to set valid value for switch_id");
+        }
+        this.switchId = switchId;
+    }
+
+    @JsonProperty("type")
+    public StatsType getStatsType() {
+        return statsType;
+    }
+
+    @JsonProperty("type")
+    public void setStatsType(StatsType statsType) {
+        if (statsType == null) {
+            throw new IllegalArgumentException("need to specify stats type");
+        }
+        this.statsType = statsType;
+    }
+}
