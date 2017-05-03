51c17f583c0a6b199aca81a4a0e64fb480dfb702
Vitaly Nazaryants <vinazaryants@mirantis.com>
2017-05-03 17:05:31 +0400

    Statistics requests

--- src/floodlight-modules/src/main/java/org/bitbucket/openkilda/floodlight/kafka/KafkaMessageCollector.java
+++ src/floodlight-modules/src/main/java/org/bitbucket/openkilda/floodlight/kafka/KafkaMessageCollector.java
@@ -35,6 +35,7 @@ import java.util.concurrent.Executors;
 import java.util.function.Function;
 
 import static java.util.stream.Collectors.toList;
+import java.util.function.Function;
 
 public class KafkaMessageCollector implements IFloodlightModule {
     private static final Logger logger = LoggerFactory.getLogger(KafkaMessageCollector.class);
