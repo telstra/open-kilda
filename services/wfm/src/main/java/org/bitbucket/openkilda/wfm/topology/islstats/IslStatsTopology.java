1efff2ee9aa185fa8da7cba2dd8ea5af3b396a83
Carmine Rimi <carmine.rimi@gmail.com>
2017-10-06 21:35:04 +1100

    Migrate IslStatsTopology to leverage AbstractTopology
    Several classes were deprecated and a new pattern introduced in the
    other topologies. The merged IslStatsTopology failed.

--- services/wfm/src/main/java/org/bitbucket/openkilda/wfm/topology/islstats/IslStatsTopology.java
+++ services/wfm/src/main/java/org/bitbucket/openkilda/wfm/topology/islstats/IslStatsTopology.java
@@ -10,78 +10,68 @@ import org.apache.storm.opentsdb.bolt.OpenTsdbBolt;
 import org.apache.storm.opentsdb.bolt.TupleOpenTsdbDatapointMapper;
 import org.apache.storm.opentsdb.client.OpenTsdbClient;
 import org.apache.storm.topology.TopologyBuilder;
-import org.bitbucket.openkilda.wfm.KafkaUtils;
+
 import org.bitbucket.openkilda.wfm.topology.AbstractTopology;
-import org.bitbucket.openkilda.wfm.topology.event.InfoEventSplitterBolt;
+import org.bitbucket.openkilda.wfm.topology.Topology;
 import org.bitbucket.openkilda.wfm.topology.islstats.bolts.IslStatsBolt;
+
+
+import java.io.File;
+
 
 public class IslStatsTopology extends AbstractTopology {
     private static Logger logger = LogManager.getLogger(IslStatsTopology.class);
 
     private final String topoName = "WFM_IslStats";
-    private final KafkaUtils kutils;
     private final int parallelism = 1;
 
     private String topic = "kilda-test";
 
-    public IslStatsTopology() {
-        this.kutils = new KafkaUtils();
+    public IslStatsTopology(File file) {
+        super(file);
     }
-
-    public IslStatsTopology(KafkaUtils kutils) {
-        this.kutils = kutils;
-    }
-
 
     public static void main(String[] args) throws Exception {
 
-        IslStatsTopology kildaTopology = new IslStatsTopology();
-        StormTopology topo = kildaTopology.createTopology();
-        String name = (args != null && args.length > 0) ?
-                args[0] : kildaTopology.topoName;
-
-        Config conf = new Config();
-        conf.setDebug(false);
-
         //If there are arguments, we are running on a cluster; otherwise, we are running locally
         if (args != null && args.length > 0) {
-            conf.setNumWorkers(kildaTopology.parallelism);
-            StormSubmitter.submitTopology(name, conf, topo);
+            File file = new File(args[1]);
+            Config conf = new Config();
+            conf.setDebug(false);
+            IslStatsTopology statsTopology = new IslStatsTopology(file);
+            StormTopology topo = statsTopology.createTopology();
+
+            conf.setNumWorkers(statsTopology.parallelism);
+            StormSubmitter.submitTopology(args[0], conf, topo);
         } else {
-            try {
-                conf.setMaxTaskParallelism(kildaTopology.parallelism);
+            File file = new File(IslStatsTopology.class.getResource(Topology.TOPOLOGY_PROPERTIES).getFile());
+            Config conf = new Config();
+            conf.setDebug(false);
+            IslStatsTopology statsTopology = new IslStatsTopology(file);
+            StormTopology topo = statsTopology.createTopology();
 
-                LocalCluster cluster = new LocalCluster();
-                cluster.submitTopology(name, conf, topo);
+            conf.setMaxTaskParallelism(3);
 
-                logger.info("sleeping");
-                while (true) {
-                    //
-                }
-//                Thread.sleep(1000 * 1000);
-//                logger.debug("shutting down topology");
-//                cluster.shutdown();
-            } catch (Exception e) {
-                logger.error("error submitting topology");
-                logger.error(e.toString());
-            }
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology(statsTopology.topologyName, conf, topo);
+
+            Thread.sleep(20000);
+            cluster.shutdown();
         }
     }
 
 
     public StormTopology createTopology() {
-        logger.debug("Building Topology - " + this.getClass().getSimpleName());
+        final String clazzName = this.getClass().getSimpleName();
+        logger.debug("Building Topology - " + clazzName );
 
         TopologyBuilder builder = new TopologyBuilder();
 
-        if (!kutils.topicExists(topic)) {
-            logger.debug(topic + " did not exist in Kafka, creating");
-            kutils.createTopics(new String[]{topic});
-        }
+        checkAndCreateTopic(topic);
 
         final String spoutName = topic + "-spout";
         logger.debug("connecting to " + topic + " topic");
-        builder.setSpout(spoutName, kutils.createKafkaSpout(topic));
+        builder.setSpout(spoutName, createKafkaSpout(topic, clazzName));
 
         final String verifyIslStatsBoltName = IslStatsBolt.class.getSimpleName();
         IslStatsBolt verifyIslStatsBolt = new IslStatsBolt();
