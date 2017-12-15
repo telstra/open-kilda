6898124a5a34271f2eac2ecce35eee98edf9aab3
Gaurav Chugh <gaurav.chugh@cloudsmartz.net>
2017-12-15 13:56:01 +0530

    Defined Hiera keys for openkilda-gui and Implemented Test cases for third party files download. Updated switch details screen : port information and on click port stats for the same.

---
+++ services/src/openkilda-gui/src/main/resources/javascript/portdetails.js
@@ -0,0 +1,66 @@
+/*<![CDATA[*/
+
+/** Below the javascript/ajax/jquery code to generate and display the switch and its corresponding details*/
+
+/** show switch details when page is loaded or
+ *  when user is redirected to this page*/
+$(document).ready(
+		function() {
+
+			var switchname = window.location.href.split("#")[1]
+
+			var tmp_anchor = '<a href="switchport#' + switchname + '">'+ switchname + '</a>';
+			
+			$("#kilda-switch-name").parent().append(tmp_anchor)
+
+			var portData = localStorage.getItem("portDetails");
+
+			var obj = JSON.parse(portData)
+
+			$("#kilda-port-name").parent().append(obj.port_name)
+
+			showSwitchData(obj); 
+			getMetric();
+
+		})
+
+/** function to retrieve and show switch details from 
+ * the switch response json object and display on the html page*/
+function showSwitchData(obj) {
+	$(".graph_div").show();
+
+	console.log(obj);
+	$(".port_details_div_status").html(obj.status);
+	$(".port_details_div_name").html(obj.port_name);
+	$(".switchdetails_div_number").html(obj.port_number);
+	$(".switchdetails_div_interface").html(obj.interface);
+
+}
+
+function getMetric() {
+	
+	
+	var linkData = localStorage.getItem("linkData");	
+	var obj = JSON.parse(linkData)
+	
+	$.ajax({
+		url : APP_CONTEXT + "/switch/metrics",
+		type : 'GET',
+		success : function(response) {
+			var metricList=response;
+			var optionHTML="";
+			for(var i=0;i<=metricList.length-1;i++){
+				optionHTML+="<option value="+metricList[i]+">"+metricList[i]+"</option>";
+							
+			}
+			
+				$("select.selectbox_menulist").html("").html(optionHTML);
+				$('#menulist').val('pen.isl.latency');
+		
+			
+		},
+		dataType : "json"
+	});
+}
+
+/* ]]> */
---
+++ services/src/openkilda-gui/src/test/java/org/openkilda/util/IConstantsTest.java
@@ -0,0 +1,33 @@
+package org.openkilda.util;
+
+
+/**
+ * The Interface IConstantsTest.
+ *
+ * @author Gaurav Chugh
+ */
+public interface IConstantsTest {
+
+	
+	/** The Constant CLASSPATH. */
+	static final String CLASSPATH = "src/main/resources";
+	
+	/** The Constant CSS_PATH. */
+	static final String CSS_PATH = "/css/";
+	
+	/** The Constant JAVASCRIPT_PATH. */
+	static final String JAVASCRIPT_PATH = "/javascript/";
+	
+	/** The Constant FONTS_PATH. */
+	static final String FONTS_PATH = "/fonts/";
+	
+	/** The Constant FILE_PATH. */
+	static final String FILE_PATH = "src/test/resources/config";
+	
+	/** The Constant JQUERY_FILE. */
+	static final String JQUERY_FILE = "jquery.min.js";
+
+	/** The Constant JQUERY_MIN_FILE. */
+	static final String JQUERY_MIN_FILE= "jquery-3.2.1.min.js";
+	
+}
