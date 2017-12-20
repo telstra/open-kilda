adf1b3ca3c33740c67cadbffe486a597549f28fb
Carmine Rimi <carmine.rimi@gmail.com>
2017-12-20 23:09:03 +1100

    MAINT: Add BaseMessage and fix Speaker test
    BaseMessage.java didn't make it into the last commit.
    
    And, the tests in Speaker weren't updated to reflect changed
    message format.
    
    There is still code in python that needs updating.

--- services/src/messaging/src/main/java/org/openkilda/messaging/command/flow/InstallOneSwitchFlow.java
+++ services/src/messaging/src/main/java/org/openkilda/messaging/command/flow/InstallOneSwitchFlow.java
@@ -39,7 +39,6 @@ import java.util.Objects;
 @JsonSerialize
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-        "command",
         TRANSACTION_ID,
         FLOW_ID,
         "cookie",
