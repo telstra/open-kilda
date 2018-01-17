e6ef2ddf0893185caed4276b192c4ca0316435af
Gaurav Chugh <gaurav.chugh@cloudsmartz.net>
2018-01-17 21:57:22 +0530

    Integrated Northbound API to get Isl links.

--- services/src/openkilda-gui/src/main/java/org/openkilda/integration/converter/IslLinkConverter.java
+++ services/src/openkilda-gui/src/main/java/org/openkilda/integration/converter/IslLinkConverter.java
@@ -3,8 +3,8 @@ package org.openkilda.integration.converter;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openkilda.integration.model.response.LinkResponse;
-import org.openkilda.integration.model.response.PathNode;
+import org.openkilda.integration.model.response.IslLink;
+import org.openkilda.integration.model.response.IslPath;
 import org.openkilda.model.IslLinkInfo;
 import org.openkilda.utility.CollectionUtil;
 
@@ -13,11 +13,11 @@ public final class IslLinkConverter {
     private IslLinkConverter() {
     }
 
-    public static List<IslLinkInfo> toIslLinksInfo(final List<LinkResponse> linksResponse) {
-        if(!CollectionUtil.isEmpty(linksResponse)) {
+    public static List<IslLinkInfo> toIslLinksInfo(final List<IslLink> islLinks) {
+        if(!CollectionUtil.isEmpty(islLinks)) {
             final List<IslLinkInfo> islLinkInfos = new ArrayList<>();
-            linksResponse.forEach(linkResponse -> {
-                IslLinkInfo islLinkInfo = toIslLinkInfo(linkResponse);
+            islLinks.forEach(islLink -> {
+                IslLinkInfo islLinkInfo = toIslLinkInfo(islLink);
 
                 boolean isSwitchRelationAdd = false;
 
@@ -47,22 +47,20 @@ public final class IslLinkConverter {
         return null;
     }
 
-    public static IslLinkInfo toIslLinkInfo(final LinkResponse linkResponse) {
+    public static IslLinkInfo toIslLinkInfo(final IslLink islLink) {
         IslLinkInfo islLinkInfo = new IslLinkInfo();
-        islLinkInfo.setAvailableBandwidth(linkResponse.getAvailableBandwidth());
-        islLinkInfo.setLatency(linkResponse.getLatencyNs());
-        islLinkInfo.setSpeed(linkResponse.getSpeed());
-        islLinkInfo.setState(linkResponse.getState());
+        islLinkInfo.setAvailableBandwidth(islLink.getAvailableBandwidth());
+        islLinkInfo.setSpeed(islLink.getSpeed());
 
-        List<PathNode> pathList = linkResponse.getPath();
-        if (pathList != null && !pathList.isEmpty()) {
-            if (pathList.get(0) != null) {
-                islLinkInfo.setSrcPort(pathList.get(0).getPortNo());
-                islLinkInfo.setSrcSwitch(pathList.get(0).getSwitchId());
+        List<IslPath> islPaths = islLink.getPath();
+        if (islPaths != null && !islPaths.isEmpty()) {
+            if (islPaths.get(0) != null) {
+                islLinkInfo.setSrcPort(islPaths.get(0).getPortNo());
+                islLinkInfo.setSrcSwitch(islPaths.get(0).getSwitchId());
             }
-            if (pathList.get(1) != null) {
-                islLinkInfo.setDstPort(pathList.get(1).getPortNo());
-                islLinkInfo.setDstSwitch(pathList.get(1).getSwitchId());
+            if (islPaths.get(1) != null) {
+                islLinkInfo.setDstPort(islPaths.get(1).getPortNo());
+                islLinkInfo.setDstSwitch(islPaths.get(1).getSwitchId());
             }
         }
         return islLinkInfo;
