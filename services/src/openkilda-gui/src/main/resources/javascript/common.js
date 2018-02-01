a2dab890bcdef3eae3ff69d642222f68b4e02b50
Gaurav Chugh <gaurav.chugh@cloudsmartz.net>
2018-02-01 10:51:30 +0530

    Created common method for metric details.

--- services/src/openkilda-gui/src/main/resources/javascript/common.js
+++ services/src/openkilda-gui/src/main/resources/javascript/common.js
@@ -1,4 +1,10 @@
 /*<![CDATA[*/
+
+
+/*Global Variable Constant*/
+var metricVarList = ["bits:Bits/sec","bytes:Bytes/sec","megabytes:MegaBytes/sec","packets:Packets/sec","drops:Drops/sec","errors:Errors/sec", "collisions:Collisions","frameerror:Frame Errors","overerror:Overruns","crcerror:CRC Errors"];
+
+
 var common = {	
 		getData:function(apiUrl,requestType){	
 		return $.ajax({url : APP_CONTEXT+apiUrl,type : requestType,dataType : "json"});							
@@ -47,12 +53,12 @@ $(document).click(function(){
 
 
 var loadGraph = {	
-		loadGraphData:function(apiUrl,requestType){	
+		loadGraphData:function(apiUrl,requestType,selMetric){	
 		return $.ajax({url : APP_CONTEXT+apiUrl,type : requestType,
 			dataType : "json",
 			error : function(errResponse) {
 				$("#wait1").css("display", "none");	
-				showStatsGraph.showStatsData(errResponse);
+				showStatsGraph.showStatsData(errResponse,selMetric);
 			}
 		});							
 	}
@@ -74,22 +80,129 @@ var graphAutoReload = {
 
  var showStatsGraph = {	
 
-	showStatsData:function(response) {	
+	showStatsData:function(response,metricVal) {	
+	
+		var metric1 = "";
+		var metric2 = "";
+		var data = response;
+		var jsonResponse = response.responseJSON;
+				
+		 var graphData = [];		
+		 
+		if(!jsonResponse) {
 			
-		var data = response
-		var graphData = [];
-		if(data.length){
-			var getValue = data[0].dps;
-			$.each(getValue, function (index, value) {	
-			  graphData.push([new Date(Number(index*1000)),value])
-			 }) 
+		    	var getValue = data[0].dps;	    	
+		    	 metric1 = data[0].metric;	
+		    	 
+		    	if(data.length == 2) {
+		    		var getVal = data[1].dps;
+		    		 metric2 = data[1].metric;
+		    	}
+		    	    
+				    if(!getValue) {
+				    	metric1 = "F";
+				    	metric2 = "R";		    	
+				    } else {
+				    	 for(i in getValue) {
+						    	
+						      var temparr = [];
+						      temparr[0] = new Date(Number(i*1000));
+						      if(metricVal == "megabytes"){
+						    	  temparr[1] = getValue[i] / 1048576;
+						      }
+						      else{
+						    	  temparr[1] = getValue[i]
+						      }
+						      
+						      if(data.length == 2) {
+						    	  if(metricVal == "megabytes"){
+						    	  	temparr[2] = getVal[i] / 1048576;
+						    	  }
+						    	  else{
+						    			temparr[2] = getVal[i];
+						    	  }
+						      }
+						      graphData.push(temparr)
+						 }
+				    }
+				    if(metric1 && metric2){
+				    	var labels = ['Time', metric1,metric2];
+				    }else if(metric1){
+				    	var labels = ['Time', metric1];
+				    }
+				    else{
+				    	var labels = ['Time', metric2];
+				    }	
+		    	
+		}else{
+			metric1 = "F";
+	    	metric2 = "R";
+			var labels = ['Time', metric1,metric2];
 		}
-		var g = new Dygraph(document.getElementById("graphdiv"), graphData,
-        {
-		    drawPoints: true,
-		    labels: ['Time', $("select.selectbox_menulist").val()]
-		});
+		
+		  
+		    
+		     var g = new Dygraph(document.getElementById("graphdiv"), graphData,
+		 	 {
+		 		      drawPoints: false,
+		 		      labels: labels,	 		      
+		 		      colors: ["#495cff","#aad200"],
+		 	  });	
 	}
+}
+
+
+var getMetricDetails = {	
+		getIslMetricData:function(response) {
+			var linkData = localStorage.getItem("linkData");
+			var obj = JSON.parse(linkData);
+			
+			var metricArray = [];			
+			metricArray = metricVarList;
+			var optionHTML = "";
+			for (var i = 0; i < metricArray.length ; i++) {
+				
+				if( metricArray[i].includes("megabytes")) {
+				} else {
+					optionHTML += "<option value=" + metricArray[i].split(":")[0] + ">"+ metricArray[i].split(":")[1] + "</option>";
+				}
+				
+			} if (obj.hasOwnProperty("flowid")) {
+				$("select.selectbox_menulist").html("").html(optionHTML);
+				$('#menulist').val('pen.flow.packets');
+			} else {
+				$("select.selectbox_menulist").html("").html(optionHTML);			
+				$('#menulist').val('bits');
+			}						
+		},
+		getFlowMetricData:function(response){
+						
+			var metricArray = [];			
+			metricArray = metricVarList;
+			var optionHTML = "";
+			for (var i = 0; i < metricArray.length ; i++) {
+				
+				if(metricArray[i].includes("bits") || metricArray[i].includes("packets") || metricArray[i].includes("bytes") || metricArray[i].includes("megabytes")) {
+					optionHTML += "<option value=" + metricArray[i].split(":")[0] + ">"+ metricArray[i].split(":")[1] + "</option>";
+				}
+			}
+			$("select.selectbox_menulist").html("").html(optionHTML);
+			$('#menulist').val('packets');
+		},
+		getPortMetricData:function(response){
+			var metricArray = [];			
+			metricArray = metricVarList;
+			var optionHTML = "";
+			for (var i = 0; i < metricArray.length ; i++) {
+				
+				if( metricArray[i].includes("megabytes")) {
+				} else{
+					optionHTML += "<option value=" + metricArray[i].split(":")[0] + ">"+ metricArray[i].split(":")[1] + "</option>";
+				}			
+			}
+			$("select.selectbox_menulist").html("").html(optionHTML);
+			$('#menulist').val('bits');
+		}
 }
 
 
