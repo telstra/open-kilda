1347525f41c6cffecdf9e61bf30822fd4b3bad1e
Carmine Rimi <carmine.rimi@gmail.com>
2018-02-04 21:52:13 +1100

    [MAINT] Initial Northbound changes for Link Props
    The initial changes are to enable GETting link properties.

--- services/src/northbound/src/main/java/org/openkilda/northbound/service/impl/LinkServiceImpl.java
+++ services/src/northbound/src/main/java/org/openkilda/northbound/service/impl/LinkServiceImpl.java
@@ -4,7 +4,9 @@ import static java.util.Base64.getEncoder;
 
 import org.openkilda.messaging.info.event.IslInfoData;
 import org.openkilda.northbound.converter.LinkMapper;
+import org.openkilda.northbound.dto.LinkPropsDto;
 import org.openkilda.northbound.dto.LinksDto;
+import org.openkilda.northbound.service.LinkPropsResult;
 import org.openkilda.northbound.service.LinkService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -32,6 +35,8 @@ public class LinkServiceImpl implements LinkService {
     private final Logger LOGGER = LoggerFactory.getLogger(LinkServiceImpl.class);
 
     private String linksUrl;
+    /** The URL to hit the Links Properties table .. ie :link_props in Neo4J */
+    private String linkPropsUrl;
     private HttpHeaders headers;
 
     /**
@@ -48,11 +53,10 @@ public class LinkServiceImpl implements LinkService {
 
     @PostConstruct
     void init() {
-        linksUrl = UriComponentsBuilder
-                .fromHttpUrl(topologyEngineRest)
-                .pathSegment("api", "v1", "topology", "links")
-                .build()
-                .toUriString();
+        linksUrl = UriComponentsBuilder.fromHttpUrl(topologyEngineRest)
+                .pathSegment("api", "v1", "topology", "links").build().toUriString();
+        linkPropsUrl = UriComponentsBuilder.fromHttpUrl(topologyEngineRest)
+                .pathSegment("api", "v1", "topology", "link", "props").build().toUriString();
         headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, authHeaderValue);
     }
@@ -66,5 +70,24 @@ public class LinkServiceImpl implements LinkService {
         return Stream.of(links)
                 .map(linkMapper::toLinkDto)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<LinkPropsDto> getLinkProps(LinkPropsDto keys) {
+        LOGGER.debug("Get link properties request received");
+        LinkPropsDto[] linkProps = restTemplate.exchange(linksUrl, HttpMethod.GET, new HttpEntity<>(headers),
+                LinkPropsDto[].class).getBody();
+        LOGGER.debug("Returned {} properties", linkProps.length);
+        return Arrays.asList(linkProps);
+    }
+
+    @Override
+    public LinkPropsResult setLinkProps(List<LinkPropsDto> linkPropsList) {
+        return null;
+    }
+
+    @Override
+    public LinkPropsResult delLinkProps(List<LinkPropsDto> linkPropsList) {
+        return null;
     }
 }
