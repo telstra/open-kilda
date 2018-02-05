f5e0d654e47a4d5d5b9868947e0d8ccd88771a0b
Nikita Cherevko <mrn1kke@gmail.com>
2018-02-05 02:43:03 +0200

    #184: fixed review comments

--- services/src/atdd/src/test/java/org/openkilda/atdd/FlowReinstallTest.java
+++ services/src/atdd/src/test/java/org/openkilda/atdd/FlowReinstallTest.java
@@ -1,9 +1,11 @@
 package org.openkilda.atdd;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.lang.StringUtils;
 import org.openkilda.SwitchesUtils;
 import org.openkilda.flow.FlowUtils;
 import org.openkilda.messaging.info.event.PathNode;
@@ -26,23 +28,20 @@ public class FlowReinstallTest {
         assertTrue("Switch should be turned on", SwitchesUtils.reviveSwitch(switchName));
     }
 
-    @Then("^flow (.*) is built through (.*) switch")
-    public void flowPathContainsSwitch(String flowId, String switchId) throws InterruptedException {
+    @Then("^flow (.*) is(.*) built through (.*) switch")
+    public void flowPathContainsSwitch(String flow, String shouldNotContain, String switchId)
+            throws InterruptedException {
         TimeUnit.SECONDS.sleep(1);
-        FlowPathPayload payload = FlowUtils.getFlowPath(FlowUtils.getFlowName(flowId));
+        FlowPathPayload payload = FlowUtils.getFlowPath(FlowUtils.getFlowName(flow));
         assertTrue("Flow path should exist", payload != null && payload.getPath() != null);
         List<PathNode> path = payload.getPath().getPath();
-        assertTrue(path.stream()
-                .anyMatch(node -> switchId.equalsIgnoreCase(node.getSwitchId())));
-    }
 
-    @Then("^flow (.*) is not built through (.*) switch")
-    public void flowPathDoesntContainSwitch(String flowId, String switchId) throws InterruptedException {
-        TimeUnit.SECONDS.sleep(1);
-        FlowPathPayload payload = FlowUtils.getFlowPath(FlowUtils.getFlowName(flowId));
-        assertTrue("Flow path should exist", payload != null && payload.getPath() != null);
-        List<PathNode> path = payload.getPath().getPath();
-        assertTrue(path.stream()
-                .noneMatch(node -> switchId.equalsIgnoreCase(node.getSwitchId())));
+        boolean contains = path.stream()
+                .anyMatch(node -> switchId.equalsIgnoreCase(node.getSwitchId()));
+        if (StringUtils.isBlank(shouldNotContain)) {
+            assertTrue(contains);
+        } else {
+            assertFalse(contains);
+        }
     }
 }
