182475ee43ee84d14ca7a53b1c02857e690fc545
Carmine Rimi <carmine.rimi@gmail.com>
2018-02-07 15:06:53 +1100

    [MAINT] Update test files for hitting NB for linkprops
    Python files for hitting NB to tickle TER / Neo4j for link props.

--- services/topology-engine-rest/app/app/tests/del_link_props.py
+++ services/topology-engine-rest/app/app/tests/del_link_props.py
@@ -8,19 +8,12 @@ The path is NB -> TER -> NEO4J.
 import requests
 from base64 import b64encode
 
-url = "http://localhost:8088/api/v1/flows"
+url = "http://localhost:8088/api/v1/link/props"
 headers = {
     'Content-Type': 'application/json',
     'correlation_id': 'deploy-flow-1',
     'Authorization': 'Basic %s' % b64encode(b"kilda:kilda").decode("ascii")
 }
-
-#
-# This models one of the first flows used by ATDD. It sends the request to teh NB API so that
-# kilda will construct the flow path rules.
-# TODO: would be better to pull from the same data, ensure code bases on synchronized..
-#       at the moment, this is hardcoded here, and ATDD has a separate source.
-#
 
 j_data = {"flowid":"c3none",
           "source":
