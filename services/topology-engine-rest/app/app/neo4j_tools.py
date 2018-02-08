eed7cb751cb24e4e6daff68f8952118194b9ebd6
Dmitry Bogun <dbogun@mirantis.com>
2018-02-08 12:40:23 +0200

    Add retry mechanism for py2neo calls
    py2neo call can throw py2neo.database.status.TransientError.
    Documentation recomend to retry request in this case. But py2neo don't
    provide any tools to make such retry.
    
    Invent simple retry mechanism via subclassing
    py2neo.database.BoltTransaction and injecting it into py2neo via
    monkeypatching.
    
    Close #254

--- services/topology-engine-rest/app/app/neo4j_tools.py
+++ services/topology-engine-rest/app/app/neo4j_tools.py
@@ -1,12 +1,71 @@
+from __future__ import absolute_import
+
+import ConfigParser
 import logging
 import os
+import time
 
+import py2neo.database
+import py2neo.database.status
 from py2neo import Graph
+
+from . import exc
 
 logger = logging.getLogger(__name__)
 
 
+class RetryConfig(object):
+    iteration = 0
+    max_attempts = 3
+    delay = 0.1
+
+    @classmethod
+    def configure(cls, config):
+        for attr, conv in (
+                ('max_attempts', int),
+                ('delay', float)):
+            key = 'retry.' + attr
+            try:
+                value = config.get('neo4j', key)
+                value = conv(value)
+            except (ConfigParser.NoOptionError, ConfigParser.NoSectionError):
+                continue
+            except TypeError as e:
+                raise exc.ConfigError('neo4j', key, e)
+
+            setattr(cls, attr, value)
+
+    def can_retry(self):
+        if self.max_attempts < 0:
+            return True
+        return self.iteration < self.max_attempts
+
+    def sleep(self):
+        self.iteration += 1
+        if 0 < self.delay:
+            time.sleep(self.delay)
+
+
+class BoltTransactionPatch(py2neo.database.BoltTransaction):
+    def run(self, statement, parameters=None, **kwparameters):
+        retry = RetryConfig()
+
+        while retry.can_retry():
+            try:
+                result = super(BoltTransactionPatch, self).run(
+                        statement, parameters, **kwparameters)
+            except py2neo.database.status.TransientError:
+                if not self.autocommit:
+                    raise
+                retry.sleep()
+                continue
+
+            return result
+
+
 def connect(config):
+    RetryConfig.configure(config)
+
     env_keys_map = {
         'host': 'neo4host',
         'login': 'neo4usr',
@@ -36,3 +95,7 @@ def connect(config):
     args['password'] = '*' * 5
     logger.info('NEO4j connect %s', uri_format(**args))
     return Graph(uri)
+
+
+# monkeypatching py2neo.database to inject our retry mechanism
+py2neo.database.BoltTransaction = BoltTransactionPatch
