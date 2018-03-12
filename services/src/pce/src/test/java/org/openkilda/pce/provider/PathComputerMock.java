0851b7a31ec87a19c1882b90d710e53c795f5624
Carmine Rimi <carmine.rimi@gmail.com>
2018-03-12 21:10:01 +1100

    Fix Make Unit failures

--- services/src/pce/src/test/java/org/openkilda/pce/provider/PathComputerMock.java
+++ services/src/pce/src/test/java/org/openkilda/pce/provider/PathComputerMock.java
@@ -45,10 +45,6 @@ public class PathComputerMock implements PathComputer {
         return 1L;
     }
 
-    @Override
-    public List<FlowInfo> getFlowInfo() {
-        return new ArrayList<>();
-    }
 
     @Override
     public ImmutablePair<PathInfoData, PathInfoData> getPath(Flow flow, Strategy strategy) {
@@ -73,6 +69,11 @@ public class PathComputerMock implements PathComputer {
         return new ImmutablePair<>(
                 path(source, destination, flow.getBandwidth()),
                 path(destination, source, flow.getBandwidth()));
+    }
+
+    @Override
+    public List<FlowInfo> getFlowInfo() {
+        return new ArrayList<>();
     }
 
     private PathInfoData path(SwitchInfoData srcSwitch, SwitchInfoData dstSwitch, int bandwidth) {
