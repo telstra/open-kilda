facfc3fb345c191af158e7dfc61589b9b56baea1
Sergii Iakovenko <siakovenko@mirantis.com>
2018-03-14 17:30:07 +0200

    Improve asserting in discovery_mechanism.feature

--- services/src/atdd-staging/src/main/java/org/openkilda/atdd/staging/steps/helpers/TopologyChecker.java
+++ services/src/atdd-staging/src/main/java/org/openkilda/atdd/staging/steps/helpers/TopologyChecker.java
@@ -1,7 +1,16 @@
 package org.openkilda.atdd.staging.steps.helpers;
 
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.beans.HasPropertyWithValue;
 import org.openkilda.atdd.staging.model.topology.TopologyDefinition;
+import org.openkilda.atdd.staging.model.topology.TopologyDefinition.Isl;
+import org.openkilda.atdd.staging.model.topology.TopologyDefinition.Switch;
+import org.openkilda.atdd.staging.service.floodlight.model.SwitchEntry;
 import org.openkilda.messaging.info.event.IslInfoData;
 import org.openkilda.messaging.info.event.PathNode;
 import org.openkilda.messaging.info.event.SwitchInfoData;
@@ -69,6 +78,57 @@ public final class TopologyChecker {
                 discoveredSrcNode.getPortNo() == linkDef.getSrcPort() &&
                 discoveredDstNode.getSwitchId().equalsIgnoreCase(linkDef.getDstSwitch().getDpId()) &&
                 discoveredDstNode.getPortNo() == linkDef.getDstPort();
+    }
+
+    public static class SwitchMatcher extends HasPropertyWithValue<SwitchInfoData> {
+
+        private Switch sw;
+
+        public SwitchMatcher(Switch sw) {
+            super("switchId", equalTo(sw.getDpId()));
+            this.sw = sw;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendValue(format("%s %s", sw.getName(), sw.getDpId()));
+        }
+    }
+
+    public static class SwitchEntryMatcher extends HasPropertyWithValue<SwitchEntry> {
+
+        private Switch sw;
+
+        public SwitchEntryMatcher(Switch sw) {
+            super("switchId", equalTo(sw.getDpId()));
+            this.sw = sw;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendValue(format("%s %s", sw.getName(), sw.getDpId()));
+        }
+    }
+
+    public static class IslMatcher extends BaseMatcher<IslInfoData> {
+
+        private Isl isl;
+
+        public IslMatcher(Isl isl) {
+            this.isl = isl;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText(format("%s %s-%d => %s %s-%d",
+                    isl.getSrcSwitch().getName(), isl.getSrcSwitch().getDpId(), isl.getSrcPort(),
+                    isl.getDstSwitch().getName(), isl.getDstSwitch().getDpId(), isl.getDstPort()));
+        }
+
+        @Override
+        public boolean matches(Object actualValue) {
+            return TopologyChecker.isIslEqual(isl, (IslInfoData) actualValue);
+        }
     }
 
     private TopologyChecker() {
