bca801020015738873f19642633b904fa7e417e3
Dmitry Bogun <dbogun@mirantis.com>
2018-04-27 11:51:41 +0300

    Fix drop kilda/base-floodlight docker image usage artefacts

--- base/build-base.sh
+++ base/build-base.sh
@@ -20,4 +20,3 @@ pushd base-java && make && popd
 pushd base-maven && make && popd
 pushd base-maven/tests && ./project.sh all && popd
 docker build -t kilda/base-ubuntu:latest kilda-base-ubuntu/
-docker build -t kilda/base-floodlight:latest base-floodlight/
