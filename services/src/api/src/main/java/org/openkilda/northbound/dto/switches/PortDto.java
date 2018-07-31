dacb5e03af61fd38f0b8523207c1251b42b4b17e
Ashish Aggarwal <ashish.aggarwal@cloudsmartz.net>
2018-07-31 17:24:12 +0530

    Exclude non-null properties in response.

--- services/src/api/src/main/java/org/openkilda/northbound/dto/switches/PortDto.java
+++ services/src/api/src/main/java/org/openkilda/northbound/dto/switches/PortDto.java
@@ -15,10 +15,12 @@
 
 package org.openkilda.northbound.dto.switches;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PortDto {
 
     @JsonProperty("switch_id")
