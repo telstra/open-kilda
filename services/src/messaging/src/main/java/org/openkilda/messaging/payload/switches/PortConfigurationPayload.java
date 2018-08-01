42722059264daa87161dd727947d428e4e8547fb
Ashish Aggarwal <ashish.aggarwal@cloudsmartz.net>
2018-08-01 14:57:33 +0530

    Fixing check style issues

--- services/src/messaging/src/main/java/org/openkilda/messaging/payload/switches/PortConfigurationPayload.java
+++ services/src/messaging/src/main/java/org/openkilda/messaging/payload/switches/PortConfigurationPayload.java
@@ -19,7 +19,6 @@ import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import lombok.Getter;
