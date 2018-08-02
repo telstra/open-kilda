469533d4e35ff80a9884c0485dbeabcd8032e556
Dmitry Bogun <dbogun@mirantis.com>
2018-08-02 11:41:50 +0300

    Remove redundant AbstractOfHandler class
    It became redundant when we have rejected FL input message processing
    model. I.e. each service subscribe to required types of messages and
    incoming message passed for each subscriber sequentially.
    
    Now we have a layer to process incoming messages in parallel i.e. on
    each type of message we have only one subscription hided inside
    InputService.

--- services/src/floodlight-modules/src/main/java/org/openkilda/floodlight/service/of/InputService.java
+++ services/src/floodlight-modules/src/main/java/org/openkilda/floodlight/service/of/InputService.java
@@ -18,33 +18,44 @@ package org.openkilda.floodlight.service.of;
 import org.openkilda.floodlight.command.CommandContext;
 import org.openkilda.floodlight.command.InputDispatchCommand;
 import org.openkilda.floodlight.model.OfInput;
-import org.openkilda.floodlight.service.AbstractOfHandler;
 import org.openkilda.floodlight.service.CommandProcessorService;
 import org.openkilda.floodlight.utils.CommandContextFactory;
+import org.openkilda.floodlight.utils.NewCorrelationContextRequired;
 
+import com.google.common.collect.ImmutableSet;
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.IFloodlightService;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class InputService extends AbstractOfHandler implements IFloodlightService {
+public class InputService implements IFloodlightService, IOFMessageListener {
+    private static Logger log = LoggerFactory.getLogger(InputService.class);
+
     private final HashMap<OFType, List<IInputTranslator>> translators = new HashMap<>();
+    private final CommandContextFactory commandContextFactory;
+
+    private final Set<String> mustHandleBefore = ImmutableSet.of();
+    private final Set<String> mustHandleAfter = ImmutableSet.of();
 
     private IFloodlightProviderService flProviderService;
     private CommandProcessorService commandProcessor;
 
     public InputService(CommandContextFactory commandContextFactory) {
-        super(commandContextFactory);
+        this.commandContextFactory = commandContextFactory;
     }
 
     /**
@@ -72,13 +83,51 @@ public class InputService extends AbstractOfHandler implements IFloodlightServic
     }
 
     @Override
-    protected boolean handle(
-            CommandContext commandContext, IOFSwitch sw, OFMessage message, FloodlightContext context) {
+    @NewCorrelationContextRequired
+    public Command receive(IOFSwitch sw, OFMessage message, FloodlightContext context) {
         final OfInput input = new OfInput(sw, message, context);  // must be constructed as early as possible
+        final String packetIdentity = String.format(
+                "(dpId: %s, xId: %s, version: %s, type: %s)",
+                sw.getId(), message.getXid(), message.getVersion(), message.getType());
 
+        log.debug("{} - receive message {}", getClass().getCanonicalName(), packetIdentity);
+        try {
+            handle(input);
+        } catch (Exception e) {
+            log.error(String.format("Unhandled exception during processing %s", packetIdentity), e);
+        }
+        return Command.CONTINUE;
+    }
+
+    private void handle(OfInput input) {
+        final CommandContext commandContext = commandContextFactory.produce();
         List<IInputTranslator> queue = translators.get(input.getType());
         commandProcessor.processLazy(new InputDispatchCommand(commandContext, commandProcessor, queue, input));
+    }
 
-        return false;
+    private void activateSubscription(IFloodlightProviderService flProviderService, OFType... desiredTypes) {
+        for (OFType target : desiredTypes) {
+            log.debug("{} activate subscription for OFMessage with OFType.{}", this.getClass().getName(), target);
+            flProviderService.addOFMessageListener(target, this);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return getClass().getName();
+    }
+
+    @Override
+    public boolean isCallbackOrderingPrereq(OFType type, String name) {
+        final boolean isMatch = mustHandleAfter.contains(name);
+        log.debug("listener ordering AFTER constraint: {} vs {} - {}", getClass().getName(), name, isMatch);
+        return isMatch;
+    }
+
+    @Override
+    public boolean isCallbackOrderingPostreq(OFType type, String name) {
+        final boolean isMatch = mustHandleBefore.contains(name);
+        log.debug("listener ordering BEFORE constraint: {} vs {} - {}", getClass().getName(), name, isMatch);
+        return isMatch;
     }
 }
