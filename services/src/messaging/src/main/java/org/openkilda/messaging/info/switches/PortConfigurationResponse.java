e6558a013f48b3f6c287e8b9024d04d000c62235
Ashish Aggarwal <ashish.aggarwal@cloudsmartz.net>
2018-08-09 11:04:14 +0530

    Update lombok configuration

--- services/src/messaging/src/main/java/org/openkilda/messaging/info/switches/PortConfigurationResponse.java
+++ services/src/messaging/src/main/java/org/openkilda/messaging/info/switches/PortConfigurationResponse.java
@@ -21,12 +21,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Getter
-@Setter
+@Data
 public class PortConfigurationResponse extends InfoData {
 
     private static final long serialVersionUID = 7332950619136252164L;
