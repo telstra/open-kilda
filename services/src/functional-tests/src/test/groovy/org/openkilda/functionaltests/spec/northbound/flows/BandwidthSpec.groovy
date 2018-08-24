3209e5870635a0f7864baf818014dc1d759d0693
Yaroslav Lobankov <ylobankov@mirantis.com>
2018-08-24 12:40:12 +0400

    Add tests to verify impossibility/possibility to exceed bw limit on ISL
    The idea of the tests is to verify impossibility/possibility to exceed
    available bandwidth on ISL when creating a flow with
    'ignore_bandwidth = false/true'.
    
    Part of #1085.

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/northbound/flows/BandwidthSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/northbound/flows/BandwidthSpec.groovy
@@ -8,8 +8,10 @@ import org.openkilda.messaging.payload.flow.FlowState
 import org.openkilda.testing.model.topology.TopologyDefinition
 import org.openkilda.testing.model.topology.TopologyDefinition.Switch
 import org.openkilda.testing.service.northbound.NorthboundService
+import org.openkilda.testing.service.topology.TopologyEngineService
 import org.openkilda.testing.tools.IslUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.client.HttpClientErrorException
 
 class BandwidthSpec extends BaseSpecification {
 
@@ -19,6 +21,8 @@ class BandwidthSpec extends BaseSpecification {
     FlowHelper flowHelper
     @Autowired
     NorthboundService northboundService
+    @Autowired
+    TopologyEngineService topologyEngineService
     @Autowired
     PathHelper pathHelper
     @Autowired
@@ -58,6 +62,55 @@ class BandwidthSpec extends BaseSpecification {
                 def bwAfterFlow = islUtils.getIslInfo(linksAfterFlow, it).availableBandwidth
                 bwAfterFlow == bwBeforeFlow - maximumBandwidth
             }
+        }
+
+        cleanup: "Delete created flow"
+        flow?.id && northboundService.deleteFlow(flow.id)
+    }
+
+    def "Unable to exceed bandwidth limit on ISL when creating a flow"() {
+        given: "Two active switches"
+        def switches = topology.getActiveSwitches()
+        def (Switch srcSwitch, Switch dstSwitch) = [switches, switches].combinations()
+                .find { Switch src, Switch dst -> src != dst }
+        assert srcSwitch && dstSwitch
+
+        when: "Create a flow with a bandwidth that exceeds available bandwidth on ISL"
+        def flow = flowHelper.randomFlow(srcSwitch, dstSwitch)
+        def possibleFlowPaths = topologyEngineService.getPaths(srcSwitch.dpId, dstSwitch.dpId)*.path
+        def allLinks = northboundService.getAllLinks()
+        def involvedBandwidths = []
+
+        possibleFlowPaths.each { path ->
+            pathHelper.getInvolvedIsls(path).each { link ->
+                involvedBandwidths.add(islUtils.getIslInfo(allLinks, link).availableBandwidth)
+            }
+        }
+        flow.maximumBandwidth = involvedBandwidths.max() + 1
+        flow.ignoreBandwidth = false
+        northboundService.addFlow(flow)
+
+        then: "Flow is not created because flow path should not be found"
+        def exc = thrown(HttpClientErrorException)
+        exc.rawStatusCode == 404
+    }
+
+    def "Able to exceed bandwidth limit on ISL when creating a flow with ignore_bandwidth = true"() {
+        given: "Two active switches"
+        def switches = topology.getActiveSwitches()
+        def (Switch srcSwitch, Switch dstSwitch) = [switches, switches].combinations()
+                .find { Switch src, Switch dst -> src != dst }
+        assert srcSwitch && dstSwitch
+
+        when: "Create a flow with a bandwidth that exceeds available bandwidth on ISL (ignore_bandwidth = true)"
+        def flow = flowHelper.randomFlow(srcSwitch, dstSwitch)
+        flow.maximumBandwidth = Integer.MAX_VALUE
+        flow.ignoreBandwidth = true
+        northboundService.addFlow(flow)
+
+        then: "Flow is successfully created"
+        Wrappers.wait(5) {
+            northboundService.getFlowStatus(flow.id).status == FlowState.UP
         }
 
         cleanup: "Delete created flow"
