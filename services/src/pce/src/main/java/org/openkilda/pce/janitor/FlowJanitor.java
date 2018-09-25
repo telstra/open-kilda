fcc44fd0888884a94e7ada15ab31e619249c0ebc
Anton Grigorev <agrigorev@mirantis.com>
2018-09-25 19:01:25 +0200

    Fix checkstyle for PCE project.

--- services/src/pce/src/main/java/org/openkilda/pce/janitor/FlowJanitor.java
+++ services/src/pce/src/main/java/org/openkilda/pce/janitor/FlowJanitor.java
@@ -12,117 +12,147 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+
 package org.openkilda.pce.janitor;
 
-import org.apache.commons.cli.*;
-import org.neo4j.driver.v1.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
 import sun.misc.BASE64Encoder;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import javax.ws.rs.client.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 /**
- * FlowJanitor holds methods that facilitate detecting and fixing issues with flows. Examples will
- * be included over time.
+ * FlowJanitor holds methods that facilitate detecting and fixing issues with flows. Examples will be included over
+ * time.
  */
 public class FlowJanitor {
 
     /**
-     * Use to get the flows that share the same cookie (eg 1 cookie, N flows)
+     * Use to get the flows that share the same cookie (eg 1 cookie, N flows).
      */
     public static final String DUPLICATE_COOKIES_QUERY =
-                    "MATCH (:switch) -[rel:flow]-> (:switch)" +
-                            " WITH rel.cookie as affected_cookie, COUNT(rel.cookie) as cookie_num" +
-                            " WHERE cookie_num > 1" +
-                            " MATCH (:switch) -[rel2:flow]-> (:switch)" +
-                            " WHERE rel2.cookie = affected_cookie" +
-                            " RETURN affected_cookie, rel2.flowid as affected_flow_id" +
-                            " ORDER BY affected_cookie ";
+            "MATCH (:switch) -[rel:flow]-> (:switch)"
+                    + " WITH rel.cookie as affected_cookie, COUNT(rel.cookie) as cookie_num"
+                    + " WHERE cookie_num > 1"
+                    + " MATCH (:switch) -[rel2:flow]-> (:switch)"
+                    + " WHERE rel2.cookie = affected_cookie"
+                    + " RETURN affected_cookie, rel2.flowid as affected_flow_id"
+                    + " ORDER BY affected_cookie ";
 
     /**
-     * Use to get the flows that share the same transit vlan (eg 1 t-vlan, N flows)
+     * Use to get the flows that share the same transit vlan (eg 1 t-vlan, N flows).
      */
     public static final String DUPLICATE_VLAN_QUERY =
-            "MATCH (:switch) -[rel:flow]-> (:switch)" +
-                    " WITH rel.transit_vlan as affected_cookie, COUNT(rel.transit_vlan) as cookie_num" +
-                    " WHERE cookie_num > 1" +
-                    " MATCH (:switch) -[rel2:flow]-> (:switch)" +
-                    " WHERE rel2.transit_vlan = affected_cookie and rel2.transit_vlan > 0" +
-                    " RETURN affected_cookie, rel.transit_vlan AS transit_vlan, rel2.flowid as affected_flow_id" +
-                    " ORDER BY affected_cookie";
+            "MATCH (:switch) -[rel:flow]-> (:switch)"
+                    + " WITH rel.transit_vlan as affected_cookie, COUNT(rel.transit_vlan) as cookie_num"
+                    + " WHERE cookie_num > 1"
+                    + " MATCH (:switch) -[rel2:flow]-> (:switch)"
+                    + " WHERE rel2.transit_vlan = affected_cookie and rel2.transit_vlan > 0"
+                    + " RETURN affected_cookie, rel.transit_vlan AS transit_vlan, rel2.flowid as affected_flow_id"
+                    + " ORDER BY affected_cookie";
 
     /**
-     * Use to get the flows that have multiple instances (ie N flows .. should be just 1 flow)
+     * Use to get the flows that have multiple instances (ie N flows .. should be just 1 flow).
      */
     public static final String DUPLICATE_FLOWS_QUERY =
-            "MATCH (:switch) -[rel:flow]-> (:switch)"+
-                    " WITH rel.flowid as affected_flow_id, COUNT(rel.flowid) as flow_num"+
-                    " WHERE flow_num > 2"+
-                    " MATCH (:switch) -[rel2:flow]-> (:switch)"+
-                    " WHERE rel2.flowid = affected_flow_id"+
-                    " RETURN affected_flow_id, rel2.cookie as affected_flow_cookie" +
-                    " ORDER BY affected_flow_id";
+            "MATCH (:switch) -[rel:flow]-> (:switch)"
+                    + " WITH rel.flowid as affected_flow_id, COUNT(rel.flowid) as flow_num"
+                    + " WHERE flow_num > 2"
+                    + " MATCH (:switch) -[rel2:flow]-> (:switch)"
+                    + " WHERE rel2.flowid = affected_flow_id"
+                    + " RETURN affected_flow_id, rel2.cookie as affected_flow_cookie"
+                    + " ORDER BY affected_flow_id";
 
     /**
-     * Use to delete a flow (ie in conjunction with DUPLICATE_FLOWS_QUERY)
+     * Use to delete a flow (ie in conjunction with DUPLICATE_FLOWS_QUERY).
      */
     public static final String DELETE_DUPLICATE_FLOW =
-            "MATCH (:switch) -[rel:flow]-> (:switch)"+
-                    " WHERE rel.flowid = %s AND rel.cookie = %d"+
-                    " DELETE rel";
+            "MATCH (:switch) -[rel:flow]-> (:switch)"
+                    + " WHERE rel.flowid = %s AND rel.cookie = %d"
+                    + " DELETE rel";
 
     public static final class Config {
-        public String neo_url;
-        public String neo_user;
-        public String neo_pswd;
-        public String nb_url;
-        public String nb_user;
-        public String nb_pswd;
+        public String neoUrl;
+        public String neoUser;
+        public String neoPswd;
+        public String nbUrl;
+        public String nbUser;
+        public String nbPswd;
         public String action;
     }
 
 
     /**
-     * @return the number of cookies that have more than one flow. This shouldn't happen, but this is
-     * here to catch scenarios where it does. '2' means two cookies have more than 1 flow each.
+     * Returns the number of cookies that have more than one flow.
+     *
+     * @return the number of cookies that have more than one flow. This shouldn't happen, but this is here to catch
+     *     scenarios where it does. '2' means two cookies have more than 1 flow each.
      */
-    public int CountDuplicateCookies() {
+    public int countDuplicateCookies() {
         throw new NotImplementedException(); // TODO: Implement Me
     }
 
     /**
-     * @return the number of cookies that have more than one flow. This shouldn't happen, but this is
-     * here to catch scenarios where it does. '2' means two cookies have more than 1 flow each.
+     * Returns the number of cookies that have more than one flow.
+     *
+     * @return the number of cookies that have more than one flow. This shouldn't happen, but this is here to catch
+     *     scenarios where it does. '2' means two cookies have more than 1 flow each.
      */
-    public List<String> FlowsWithDuplicateCookies(boolean verbose) {
+    public List<String> flowsWithDuplicateCookies(boolean verbose) {
         throw new NotImplementedException(); // TODO: Implement Me
     }
 
-
+    /**
+     * Updates flows.
+     *
+     * @param config the flow janitor config
+     * @param flowsToUpdate list of flows to update
+     */
     public static final void updateFlows(FlowJanitor.Config config, List<String> flowsToUpdate) {
-        String authString = config.nb_user + ":" + config.nb_pswd;
+        String authString = config.nbUser + ":" + config.nbPswd;
         String authStringEnc = new BASE64Encoder().encode(authString.getBytes());
 
         Client client = ClientBuilder.newClient();
 
-        for (String flowid : flowsToUpdate){
+        for (String flowid : flowsToUpdate) {
             /*
              * Get the Flows .. call NB for each
              */
             try {
                 TimeUnit.SECONDS.sleep(1);
-            } catch (Exception e) {}
+            } catch (Exception e) {
+                //ignore it
+            }
 
             System.out.println("RUNNING: flowid = " + flowid);
 
-            WebTarget webTarget = client.target(config.nb_url + "/api/v1/flows").path(flowid);
+            WebTarget webTarget = client.target(config.nbUrl + "/api/v1/flows").path(flowid);
             Invocation.Builder invocationBuilder =
                     webTarget.request(MediaType.APPLICATION_JSON)
-                    .header("Authorization", "Basic " + authStringEnc);
+                            .header("Authorization", "Basic " + authStringEnc);
 
             Response response = invocationBuilder.get(Response.class);
 
@@ -137,12 +167,13 @@ public class FlowJanitor {
              * Call update Flow .. add to description
              */
             String[] split = output.split("PUSHED FLOW");
-            if (split.length == 2)
+            if (split.length == 2) {
                 output = split[0] + "PUSHED FLOW. FIX cookie dupe." + split[1];
+            }
 
 
             // LOOP
-            response = invocationBuilder.put(Entity.entity(output,MediaType.APPLICATION_JSON));
+            response = invocationBuilder.put(Entity.entity(output, MediaType.APPLICATION_JSON));
 
             if (response.getStatus() != 200) {
                 System.out.println("FAILURE: flowid = " + flowid + "; response = " + response.getStatus());
@@ -152,8 +183,6 @@ public class FlowJanitor {
 
     /**
      * With the right URL, username, password, and method keyword, the methods above can be called.
-     *
-     * @param args
      */
     public static void main(String[] args) {
         Options options = new Options();
@@ -172,7 +201,7 @@ public class FlowJanitor {
                 .desc("The Neo4J password - e.g. kilda").build());
 
         options.addOption(Option.builder("a").required(true).hasArg().longOpt("action")
-                .desc("The action to take - e.g. CountDuplicateCookies").build());
+                .desc("The action to take - e.g. ountDuplicateCookies").build());
         options.addOption(Option.builder("v").required(false).longOpt("verbose")
                 .desc("Where appropriate, return a verbose response").build());
 
@@ -183,21 +212,21 @@ public class FlowJanitor {
         try {
             commandLine = parser.parse(options, args);
             FlowJanitor.Config config = new FlowJanitor.Config();
-            config.neo_url = commandLine.getOptionValue("url");
-            config.neo_user = commandLine.getOptionValue("u");
-            config.neo_pswd = commandLine.getOptionValue("p");
-            config.nb_url = commandLine.getOptionValue("nburl");
-            config.nb_user = commandLine.getOptionValue("nbu");
-            config.nb_pswd = commandLine.getOptionValue("nbp");
+            config.neoUrl = commandLine.getOptionValue("url");
+            config.neoUser = commandLine.getOptionValue("u");
+            config.neoPswd = commandLine.getOptionValue("p");
+            config.nbUrl = commandLine.getOptionValue("nburl");
+            config.nbUser = commandLine.getOptionValue("nbu");
+            config.nbPswd = commandLine.getOptionValue("nbp");
             config.action = commandLine.getOptionValue("a");
 
-            driver = GraphDatabase.driver(config.neo_url, AuthTokens.basic(config.neo_user, config.neo_pswd));
+            driver = GraphDatabase.driver(config.neoUrl, AuthTokens.basic(config.neoUser, config.neoPswd));
 
             if (config.action.equals("DeDupeFlows")) {
 
                 Session session = driver.session();
                 StatementResult result = session.run(DUPLICATE_FLOWS_QUERY);
-                Map<String,List<Long>> flowsToUpdate = new HashMap<>();
+                Map<String, List<Long>> flowsToUpdate = new HashMap<>();
                 for (Record record : result.list()) {
                     String flowid = record.get("affected_flow_id").asString();
                     List<Long> priors = flowsToUpdate.computeIfAbsent(flowid, empty -> new ArrayList<>());
@@ -209,21 +238,23 @@ public class FlowJanitor {
 
                 System.out.println("Will De-Dupe the Flows");
 
-                String authString = config.nb_user + ":" + config.nb_pswd;
+                String authString = config.nbUser + ":" + config.nbPswd;
                 String authStringEnc = new BASE64Encoder().encode(authString.getBytes());
                 Client client = ClientBuilder.newClient();
 
-                for (String flowid : flowsToUpdate.keySet()){
+                for (String flowid : flowsToUpdate.keySet()) {
                     /*
                      * Get the Flows .. call NB for each
                      */
                     try {
                         TimeUnit.SECONDS.sleep(1);
-                    } catch (Exception e) {}
+                    } catch (Exception e) {
+                        //ignore it
+                    }
 
                     System.out.println("RUNNING: flowid = " + flowid);
 
-                    WebTarget webTarget = client.target(config.nb_url + "flows").path(flowid);
+                    WebTarget webTarget = client.target(config.nbUrl + "flows").path(flowid);
                     Invocation.Builder invocationBuilder =
                             webTarget.request(MediaType.APPLICATION_JSON)
                                     .header("Authorization", "Basic " + authStringEnc);
@@ -238,7 +269,7 @@ public class FlowJanitor {
                     String output = response.readEntity(String.class);
                     System.out.println("output = " + output);
                     System.exit(0);
-                    
+
                 }
 
             } else {
@@ -257,18 +288,19 @@ public class FlowJanitor {
                 FlowJanitor.updateFlows(config, flowsToUpdate);
 
             }
-//            NeoDriver target = new NeoDriver(driver);
-//            System.out.println("target.getSwitches() = " + target.getSwitches());
+            // NeoDriver target = new NeoDriver(driver);
+            // System.out.println("target.getSwitches() = " + target.getSwitches());
 
         } catch (ParseException exception) {
             System.out.print("Parse error: ");
             System.out.println(exception.getMessage());
             // automatically generate the help statement
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp( "FlowJanitor", options );
+            formatter.printHelp("FlowJanitor", options);
         } finally {
-            if (driver != null)
+            if (driver != null) {
                 driver.close();
+            }
         }
 
     }
