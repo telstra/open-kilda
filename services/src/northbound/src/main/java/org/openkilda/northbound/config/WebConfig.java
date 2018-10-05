415f00faa3b9a2640146170d8b664a462b85bde0
Nikita Cherevko <mrn1kke@gmail.com>
2018-10-05 16:24:34 +0300

    #405: Refactored FeatureToggles, removed deprecated code, fixed tests

--- services/src/northbound/src/main/java/org/openkilda/northbound/config/WebConfig.java
+++ services/src/northbound/src/main/java/org/openkilda/northbound/config/WebConfig.java
@@ -21,13 +21,9 @@ import org.openkilda.northbound.utils.RequestCorrelationFilter;
 import org.openkilda.northbound.utils.async.CompletableFutureReturnValueHandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -37,7 +33,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import javax.annotation.PostConstruct;
 
@@ -102,19 +97,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ExtraAuthInterceptor extraAuthInterceptor() {
         return new ExtraAuthInterceptor();
-    }
-
-    /**
-     * Rest template for performing REST calls.
-     */
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        RestTemplate restTemplate = builder.build();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
-        restTemplate.getMessageConverters().add(converter);
-
-        return restTemplate;
     }
 
     @Bean
