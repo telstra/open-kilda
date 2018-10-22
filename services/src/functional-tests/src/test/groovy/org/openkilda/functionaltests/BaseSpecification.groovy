0657a5828681f245333338b1f9e5cae012c1bf50
Roman Tretiak <rtretiak@mirantis.com>
2018-10-22 16:53:33 +0300

    1475: Added preparation test as a healthcheck for the whole test suite

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/BaseSpecification.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/BaseSpecification.groovy
@@ -7,8 +7,10 @@ import org.openkilda.functionaltests.extension.fixture.SetupOnce
 import org.openkilda.functionaltests.extension.healthcheck.HealthCheck
 import org.openkilda.functionaltests.helpers.FlowHelper
 import org.openkilda.functionaltests.helpers.Wrappers
+import org.openkilda.messaging.info.event.IslChangeType
 import org.openkilda.messaging.payload.flow.FlowState
 import org.openkilda.testing.model.topology.TopologyDefinition
+import org.openkilda.testing.service.floodlight.FloodlightService
 import org.openkilda.testing.service.northbound.NorthboundService
 
 import org.springframework.beans.factory.annotation.Autowired
@@ -26,6 +28,8 @@ class BaseSpecification extends SpringSpecification implements SetupOnce {
     TopologyDefinition topologyDefinition
     @Autowired
     NorthboundService northbound
+    @Autowired
+    FloodlightService floodlight
 
     /**
      * Use this instead of setupSpec in order to have access to Spring Context and do actions BeforeClass.
@@ -46,7 +50,38 @@ class BaseSpecification extends SpringSpecification implements SetupOnce {
     }
 
     @HealthCheck
+    def "Given hardware topology is suitable for running the test suite"() {
+        requireProfiles("hardware")
+
+        expect: "Topology and overall kilda state are suitable for running further test suite"
+        def links = northbound.getAllLinks()
+        verifyAll {
+            northbound.activeSwitches.size() == topologyDefinition.activeSwitches.size()
+            northbound.allFlows.empty
+            northbound.allLinkProps.empty
+            links.findAll {
+                it.state == IslChangeType.DISCOVERED
+            }.size() == topologyDefinition.islsForActiveSwitches.size() * 2
+            links.findAll { it.state == IslChangeType.FAILED }.empty
+        }
+
+        and: "More detailed inspection also reports no issues"
+        verifyAll {
+            links.findAll { it.availableBandwidth != it.speed }.empty
+            topologyDefinition.activeSwitches.findAll {
+                def rules = northbound.validateSwitchRules(it.dpId)
+                !rules.excessRules.empty || !rules.missingRules.empty
+            }.empty
+            topologyDefinition.activeSwitches.findAll {
+                it.ofVersion != "OF_12" && !floodlight.getMeters(it.dpId).isEmpty()
+            }.empty
+        }
+    }
+
+    @HealthCheck
     def "Create casual flow to ensure Kilda's up state"() {
+        requireProfiles("virtual")
+
         when: "Create a flow"
         def flow = flowHelper.randomFlow(topologyDefinition.activeSwitches[0], topologyDefinition.activeSwitches[1])
         northbound.addFlow(flow)
@@ -56,6 +91,8 @@ class BaseSpecification extends SpringSpecification implements SetupOnce {
 
         and: "remove the flow"
         northbound.deleteFlow(flow.id)
-        Wrappers.wait(WAIT_OFFSET) { northbound.getAllLinks().every { it.availableBandwidth == it.speed } }
+        Wrappers.wait(WAIT_OFFSET) {
+            northbound.getAllLinks().every { it.availableBandwidth == it.speed }
+        }
     }
 }
