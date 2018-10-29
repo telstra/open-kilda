8465e41e87c2b133dbbf1015f4cae6cd2e9de2ac
Anton Grigorev <strannik.anton@gmail.com>
2018-10-29 16:35:31 +0100

    Fix NB checkstyle.

--- services/src/northbound/src/test/java/org/openkilda/northbound/controller/NorthboundBaseTest.java
+++ services/src/northbound/src/test/java/org/openkilda/northbound/controller/NorthboundBaseTest.java
@@ -1,3 +1,18 @@
+/* Copyright 2018 Telstra Open Source
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package org.openkilda.northbound.controller;
 
 import org.junit.AfterClass;
@@ -11,30 +26,30 @@ import java.io.File;
 
 /**
  * Add common test functionality here.
- *
  * At present, a dependency on Neo4J has been added.
  */
-public class NorthboundBaseTest {
+public abstract class NorthboundBaseTest {
 
     /*
      * The neo4j block was added due to new dependency in FlowServiceImpl
      */
     private static GraphDatabaseService graphDb;
-    private static final File databaseDirectory = new File( "target/neo4j-test-db" );
+    private static final File databaseDirectory = new File("target/neo4j-test-db");
 
     @BeforeClass
     public static void setUpOnce() throws Exception {
-        FileUtils.deleteRecursively( databaseDirectory );       // delete neo db file
+        FileUtils.deleteRecursively(databaseDirectory);       // delete neo db file
 
         // This next area enables Kilda to connect to the local db
         BoltConnector bolt = new BoltConnector("0");
         graphDb = new GraphDatabaseFactory()
-                .newEmbeddedDatabaseBuilder( databaseDirectory )
-                .setConfig( bolt.type, "BOLT" )
-                .setConfig( bolt.enabled, "true" )
-                .setConfig( bolt.listen_address, "localhost:7878" )
+                .newEmbeddedDatabaseBuilder(databaseDirectory)
+                .setConfig(bolt.type, "BOLT")
+                .setConfig(bolt.enabled, "true")
+                .setConfig(bolt.listen_address, "localhost:7878")
                 .newGraphDatabase();
     }
+
     @AfterClass
     public static void teatDownOnce() {
         graphDb.shutdown();
