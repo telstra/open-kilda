6b3446ebd82c2f69830fa8f1c40d63acef470267
Roman Tretiak <rtretiak@mirantis.com>
2018-11-09 18:04:43 +0200

    1563: Add functional test for default rules

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/northbound/switches/SwitchRulesSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/northbound/switches/SwitchRulesSpec.groovy
@@ -6,12 +6,16 @@ import org.openkilda.functionaltests.BaseSpecification
 import org.openkilda.functionaltests.helpers.Wrappers
 import org.openkilda.messaging.command.switches.DeleteRulesAction
 import org.openkilda.messaging.payload.flow.FlowState
+import org.openkilda.testing.Constants.DefaultRule
 import org.openkilda.testing.model.topology.TopologyDefinition
 import org.openkilda.testing.model.topology.TopologyDefinition.Switch
 import org.openkilda.testing.service.lockkeeper.LockKeeperService
 import org.openkilda.testing.service.northbound.NorthboundService
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import spock.lang.Ignore
+import spock.lang.Unroll
 import spock.lang.Narrative
 
 import java.util.concurrent.TimeUnit
@@ -29,12 +33,27 @@ class SwitchRulesSpec extends BaseSpecification {
     @Autowired
     LockKeeperService lockKeeperService
 
-    def "Default rules are installed on a new switch connected to the controller"() {
+    @Value('${reroute.delay}')
+    int rerouteDelay
+    @Value('${discovery.timeout}')
+    int discoveryTimeout
+
+    @Unroll("Default rules are installed on #sw.ofVersion switch(#sw.dpId)")
+    def "Default rules are installed on switches"() {
+        expect: "Default rules are installed on the #sw.ofVersion switch"
+        def cookies = northboundService.getSwitchRules(sw.dpId).flowEntries*.cookie
+        cookies.sort() == expectedRules*.cookie.sort()
+
+        where:
+        sw << uniqueSwitches
+        expectedRules = sw.ofVersion == "OF_12" ? [DefaultRule.VERIFICATION_BROADCAST_RULE] : DefaultRule.values()
+    }
+
+    @Unroll("Default rules are installed on a new #sw.ofVersion switch(#sw.dpId) when connecting it to the controller")
+    def "Default rules are installed when a new switch is connected"() {
         requireProfiles("virtual")
 
         given: "A switch with no rules installed and not connected to the controller"
-        def sw = topology.getActiveSwitches().first()
-        def defaultRules = northboundService.getSwitchRules(sw.dpId).flowEntries.sort { it.cookie }
         northboundService.deleteSwitchRules(sw.dpId, DeleteRulesAction.DROP_ALL)
         Wrappers.wait(WAIT_OFFSET) { assert northboundService.getSwitchRules(sw.dpId).flowEntries.isEmpty() }
 
@@ -45,22 +64,16 @@ class SwitchRulesSpec extends BaseSpecification {
         lockKeeperService.reviveSwitch(sw.dpId)
         Wrappers.wait(WAIT_OFFSET) { assert sw.dpId in northboundService.getActiveSwitches()*.switchId }
 
-        then: "Default rules are installed on the switch"
-        def actualRules = northboundService.getSwitchRules(sw.dpId).flowEntries.sort { it.cookie }
-        actualRules.size() == defaultRules.size()
-        [actualRules, defaultRules].transpose().each { actual, expected ->
-            verifyAll {
-                actual.cookie == expected.cookie
-                actual.tableId == expected.tableId
-                actual.version == expected.version
-                actual.priority == expected.priority
-                actual.idleTimeout == expected.idleTimeout
-                actual.hardTimeout == expected.hardTimeout
-                actual.match == expected.match
-                actual.instructions == expected.instructions
-                actual.flags == expected.flags
-            }
-        }
+        then: "The switch is really connected to the controller"
+        Wrappers.wait(WAIT_OFFSET) { sw.dpId in northboundService.getActiveSwitches()*.switchId }
+
+        and: "Default rules are installed on the switch"
+        def cookies = northboundService.getSwitchRules(sw.dpId).flowEntries*.cookie
+        cookies.sort() == expectedRules*.cookie.sort()
+
+        where:
+        sw << uniqueSwitches
+        expectedRules = sw.ofVersion == "OF_12" ? [DefaultRule.VERIFICATION_BROADCAST_RULE] : DefaultRule.values()
     }
 
     def "Pre-installed rules are not deleted from a new switch connected to the controller"() {
@@ -115,5 +128,11 @@ class SwitchRulesSpec extends BaseSpecification {
         Wrappers.wait(WAIT_OFFSET) {
             northboundService.getSwitchRules(srcSwitch.dpId).flowEntries.size() == defaultRules.size()
         }
+    }
+
+    List<Switch> getUniqueSwitches() {
+        def nbSwitches = northbound.getAllSwitches()
+        topology.getActiveSwitches()
+                .unique { sw -> [nbSwitches.find { it.switchId == sw.dpId }.description, sw.ofVersion].sort() }
     }
 }
