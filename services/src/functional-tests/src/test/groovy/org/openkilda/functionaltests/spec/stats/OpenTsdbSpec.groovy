8db8559e63e4bf5b194f5c4fd49c5c71e30d8c4f
Sergey Nikitin <snikitin@miranis.com>
2018-12-11 16:59:12 +0400

    Collect statistics from meters
    Several metrics were added to OpenTSDB.
    
    Metrics for flows:
    
    - pen.flow.meter.bits - meterBand bits
    - pen.flow.meter.bytes - meterBand bytes
    - pen.flow.meter.packets - meterBand packets
    Tags: cookie, switchid, flowid, direction, meterid
    
    Metrics for default rule meters:
    
    - pen.switch.flow.system.meter.packets - meterBand packets
    - pen.switch.flow.system.meter.bytes - meterBand bytes
    - pen.switch.flow.system.meter.bits - meterBand bits
    Tags: cookie (in Hex), switchid, meterid
    
    Closes #1573

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/stats/OpenTsdbSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/stats/OpenTsdbSpec.groovy
@@ -1,6 +1,7 @@
 package org.openkilda.functionaltests.spec.stats
 
 import org.openkilda.functionaltests.BaseSpecification
+import org.openkilda.testing.Constants.DefaultRule
 
 import groovy.time.TimeCategory
 import spock.lang.Issue
@@ -27,6 +28,31 @@ class OpenTsdbSpec extends BaseSpecification {
                 uniqueSwitches.collect { [switchid: it.dpId.toOtsdFormat()] }].combinations()
                 + [["pen.isl.latency"], uniqueSwitches.collect { [src_switch: it.dpId.toOtsdFormat()] }].combinations()
                 + [["pen.isl.latency"], uniqueSwitches.collect { [dst_switch: it.dpId.toOtsdFormat()] }].combinations())
+    }
+
+    @Unroll("Stats are being logged for metric:#metric, tags:#tags")
+    def "Stats for default rule meters"(metric, tags) {
+        requireProfiles("hardware")
+        
+        expect: "At least 1 result in the past 2 minutes"
+        otsdb.query(2.minutes.ago, metric, tags).dps.size() > 0
+        where:
+        [metric, tags] << ([
+                ["pen.switch.flow.system.meter.packets", "pen.switch.flow.system.meter.bytes",
+                 "pen.switch.flow.system.meter.bits"],
+                [[cookieHex: String.format("%X", DefaultRule.VERIFICATION_BROADCAST_RULE.cookie)],
+                 [cookieHex: String.format("%X", DefaultRule.VERIFICATION_UNICAST_RULE.cookie)]]
+        ].combinations())
+    }
+
+    @Unroll("Stats are being logged for metric:#metric")
+    def "Stats for flow meters"(metric) {
+        requireProfiles("hardware")
+
+        expect: "At least 1 result in the past 2 minutes"
+        otsdb.query(2.minutes.ago, metric, [:]).dps.size() > 0
+        where:
+        metric << ["pen.flow.meter.packets", "pen.flow.meter.bytes", "pen.flow.meter.bits"]
     }
 
     def getUniqueSwitches() {
