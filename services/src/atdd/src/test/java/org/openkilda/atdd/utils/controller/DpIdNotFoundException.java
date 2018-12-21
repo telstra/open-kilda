f4fcad4f894f4e2bd29f648deeb62dd8d86567a9
Roman Tretiak <rtretiak@mirantis.com>
2018-12-21 15:22:05 +0200

    Remove atdd module from project

--- services/src/atdd/src/test/java/org/openkilda/atdd/utils/controller/DpIdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.openkilda.atdd.utils.controller;
-
-public class DpIdNotFoundException extends Exception {
-    public DpIdNotFoundException() {
-        super("DpId not found");
-    }
-}
