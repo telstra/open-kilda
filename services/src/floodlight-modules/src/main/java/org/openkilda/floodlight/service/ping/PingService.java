70d6c4b4b5e9b1f87c242373655c903c06cfaa63
Sergey Nikitin <snikitin@miranis.com>
2018-12-28 15:15:58 +0400

    Added ability to collect statistics for default rules.
    New metrics will be send to OpenTSDB:
    
    pen.switch.flow.system.packets
    pen.switch.flow.system.bytes
    pen.switch.flow.system.bits
    
    Closes: #1572

--- services/src/floodlight-modules/src/main/java/org/openkilda/floodlight/service/ping/PingService.java
+++ services/src/floodlight-modules/src/main/java/org/openkilda/floodlight/service/ping/PingService.java
@@ -22,6 +22,7 @@ import org.openkilda.floodlight.service.of.InputService;
 import org.openkilda.floodlight.switchmanager.ISwitchManager;
 import org.openkilda.floodlight.utils.DataSignature;
 import org.openkilda.messaging.model.Ping;
+import org.openkilda.model.Cookie;
 
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
@@ -39,7 +40,7 @@ import org.projectfloodlight.openflow.types.U64;
 import java.util.Map;
 
 public class PingService implements IService {
-    public static final U64 OF_CATCH_RULE_COOKIE = U64.of(ISwitchManager.VERIFICATION_UNICAST_RULE_COOKIE);
+    public static final U64 OF_CATCH_RULE_COOKIE = U64.of(Cookie.VERIFICATION_UNICAST_RULE_COOKIE);
     private static final String NET_L3_ADDRESS = "127.0.0.2";
     private static final int NET_L3_PORT = PathVerificationService.VERIFICATION_PACKET_UDP_PORT + 1;
     private static final byte NET_L3_TTL = 96;
