47c7d5444f9ed0ed6044175d3251290ea03123cf
Sergey Nikitin <snikitin@miranis.com>
2019-01-22 14:21:52 +0400

    Changed log level of FlowNotFound exception from Error to Warn
    New LogLevel enum was introduced
    
    Closes: #1565

---
+++ services/src/messaging/src/main/java/org/openkilda/messaging/error/ClientErrorMessage.java
@@ -0,0 +1,64 @@
+/* Copyright 2019 Telstra Open Source
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.openkilda.messaging.error;
+
+import static org.openkilda.messaging.Utils.CORRELATION_ID;
+import static org.openkilda.messaging.Utils.DESTINATION;
+import static org.openkilda.messaging.Utils.PAYLOAD;
+import static org.openkilda.messaging.Utils.TIMESTAMP;
+
+import org.openkilda.messaging.Destination;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+/**
+ * Class represents client error message.
+ */
+@JsonSerialize
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder(value = {
+        DESTINATION,
+        PAYLOAD,
+        TIMESTAMP,
+        CORRELATION_ID})
+public class ClientErrorMessage extends ErrorMessage {
+
+    /**
+     * Data of the client error message.
+     */
+    @JsonProperty(PAYLOAD)
+    private ErrorData data;
+
+    /**
+     * Instance constructor.
+     *
+     * @param data          error message payload
+     * @param timestamp     timestamp value
+     * @param correlationId message correlation id
+     * @param destination   message destination
+     */
+    @JsonCreator
+    public ClientErrorMessage(@JsonProperty(PAYLOAD) final ErrorData data,
+                              @JsonProperty(TIMESTAMP) final long timestamp,
+                              @JsonProperty(CORRELATION_ID) final String correlationId,
+                              @JsonProperty(DESTINATION) final Destination destination) {
+        super(data, timestamp, correlationId, destination);
+    }
+}
