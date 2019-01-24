6ad87cc65ed560242ac60ba56f48c8f6f23d9617
Roman Tretiak <rtretiak@mirantis.com>
2019-01-24 18:46:50 +0200

    Fix stats test for default rules

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/stats/OpenTsdbSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/stats/OpenTsdbSpec.groovy
@@ -27,25 +27,12 @@ class OpenTsdbSpec extends BaseSpecification {
                  "pen.switch.tx-bytes", "pen.switch.tx-bits", "pen.switch.tx-packets"],
                 uniqueSwitches.collect { [switchid: it.dpId.toOtsdFormat()] }].combinations()
                 + [["pen.isl.latency"], uniqueSwitches.collect { [src_switch: it.dpId.toOtsdFormat()] }].combinations()
-                + [["pen.isl.latency"], uniqueSwitches.collect { [dst_switch: it.dpId.toOtsdFormat()] }].combinations())
+                + [["pen.isl.latency"], uniqueSwitches.collect { [dst_switch: it.dpId.toOtsdFormat()] }].combinations()
+                + [["pen.switch.flow.system.packets", "pen.switch.flow.system.bytes", "pen.switch.flow.system.bits"],
+                   [[cookieHex: DefaultRule.VERIFICATION_BROADCAST_RULE.toHexString()]]].combinations())
     }
 
     def getUniqueSwitches() {
         topology.activeSwitches.unique { it.ofVersion }
-    }
-
-    @Unroll("Stats are being logged for metric:#metric, tags:#tags")
-    def "Stats for default rules"(metric, tags) {
-        expect: "At least 1 result in the past 2 minutes"
-        otsdb.query(2.minutes.ago, metric, tags).dps.size() > 0
-
-        where:
-        [metric, tags] << ([
-                ["pen.switch.flow.system.packets", "pen.switch.flow.system.bytes", "pen.switch.flow.system.bits"],
-                [[cookieHex: DefaultRule.DROP_RULE.toHexString()],
-                 [cookieHex: DefaultRule.VERIFICATION_BROADCAST_RULE.toHexString()],
-                 [cookieHex: DefaultRule.VERIFICATION_UNICAST_RULE.toHexString()],
-                 [cookieHex: DefaultRule.DROP_LOOP_RULE.toHexString()]]
-        ].combinations())
     }
 }
