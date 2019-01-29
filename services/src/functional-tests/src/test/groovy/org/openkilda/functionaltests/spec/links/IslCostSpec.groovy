1256e5de39bd9f696a7ee9b0fd593105cedcf37a
Yaroslav Lobankov <ylobankov@mirantis.com>
2019-01-29 18:42:36 +0300

    Add test to verify ISL cost increase due to port down action
    Closes #1948.

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/links/IslCostSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/links/IslCostSpec.groovy
@@ -6,37 +6,131 @@ import static org.openkilda.testing.Constants.WAIT_OFFSET
 import org.openkilda.functionaltests.BaseSpecification
 import org.openkilda.functionaltests.helpers.Wrappers
 import org.openkilda.messaging.info.event.IslChangeType
+import org.openkilda.northbound.dto.links.LinkPropsDto
 import org.openkilda.testing.service.lockkeeper.model.ASwitchFlow
+
+import org.springframework.beans.factory.annotation.Value
+import spock.lang.Shared
+import spock.lang.Unroll
 
 class IslCostSpec extends BaseSpecification {
 
-    def "ISL cost is NOT increased due to connection loss between switches (not port down)"() {
-        given: "ISL going through a-switch"
+    @Shared
+    @Value('${isl.cost.when.port.down}')
+    int islCostWhenPortDown
+
+    def "ISL cost is increased due to bringing port down on a switch (ISL cost < isl.cost.when.port.down)"() {
+        given: "Active forward and reverse ISLs with equal cost"
+        def isl = topology.islsForActiveSwitches.first()
+        def reverseIsl = islUtils.reverseIsl(isl)
+
+        int islCost = islUtils.getIslCost(isl)
+        assert islUtils.getIslCost(reverseIsl) == islCost
+
+        when: "Bring port down on the source switch"
+        northbound.portDown(isl.srcSwitch.dpId, isl.srcPort)
+
+        then: "Status of forward and reverse ISLs becomes 'FAILED'"
+        Wrappers.wait(WAIT_OFFSET) {
+            def links = northbound.getAllLinks()
+            assert islUtils.getIslInfo(links, isl).get().state == IslChangeType.FAILED
+            assert islUtils.getIslInfo(links, reverseIsl).get().state == IslChangeType.FAILED
+        }
+
+        and: "Cost of forward and reverse ISLs after bringing port down is increased"
+        islUtils.getIslCost(isl) == islCostWhenPortDown + islCost
+        islUtils.getIslCost(reverseIsl) == islCostWhenPortDown + islCost
+
+        and: "Bring port up on the source switch and reset costs"
+        northbound.portUp(isl.srcSwitch.dpId, isl.srcPort)
+        database.resetCosts()
+        Wrappers.wait(discoveryTimeout + WAIT_OFFSET) {
+            def links = northbound.getAllLinks()
+            assert islUtils.getIslInfo(links, isl).get().state == IslChangeType.DISCOVERED
+            assert islUtils.getIslInfo(links, reverseIsl).get().state == IslChangeType.DISCOVERED
+        }
+    }
+
+    @Unroll
+    def "ISL cost is NOT increased due to bringing port down on a switch \
+(ISL cost #condition isl.cost.when.port.down)"() {
+        given: "Active forward and reverse ISLs"
+        def isl = topology.islsForActiveSwitches.first()
+        def reverseIsl = islUtils.reverseIsl(isl)
+
+        and: "Forward ISL has cost #condition isl.cost.when.port.down"
+        def linkProps = [new LinkPropsDto(isl.srcSwitch.dpId.toString(), isl.srcPort, isl.dstSwitch.dpId.toString(),
+                isl.dstPort, ["cost": cost.toString()])]
+        northbound.updateLinkProps(linkProps)
+
+        int islCost = islUtils.getIslCost(isl)
+        int reverseIslCost = islUtils.getIslCost(reverseIsl)
+        assert islCost != reverseIslCost
+
+        when: "Bring port down on the source switch"
+        northbound.portDown(isl.srcSwitch.dpId, isl.srcPort)
+
+        then: "Status of forward and reverse ISLs becomes 'FAILED'"
+        Wrappers.wait(WAIT_OFFSET) {
+            def links = northbound.getAllLinks()
+            assert islUtils.getIslInfo(links, isl).get().state == IslChangeType.FAILED
+            assert islUtils.getIslInfo(links, reverseIsl).get().state == IslChangeType.FAILED
+        }
+
+        and: "Cost of forward ISL after bringing port down is NOT increased"
+        islUtils.getIslCost(isl) == islCost
+
+        and: "Cost of reverse ISL after bringing port down is increased"
+        islUtils.getIslCost(reverseIsl) == islCostWhenPortDown + reverseIslCost
+
+        and: "Bring port up on the source switch, delete link props and reset costs"
+        northbound.portUp(isl.srcSwitch.dpId, isl.srcPort)
+        northbound.deleteLinkProps(northbound.getAllLinkProps())
+        database.resetCosts()
+        Wrappers.wait(discoveryTimeout + WAIT_OFFSET) {
+            def links = northbound.getAllLinks()
+            assert islUtils.getIslInfo(links, isl).get().state == IslChangeType.DISCOVERED
+            assert islUtils.getIslInfo(links, reverseIsl).get().state == IslChangeType.DISCOVERED
+        }
+
+        where:
+        cost                    | condition
+        islCostWhenPortDown     | "=="
+        islCostWhenPortDown + 1 | ">"
+    }
+
+    def "ISL cost is NOT increased due to failing connection between switches (not port down)"() {
+        given: "Active ISL going through a-switch"
         def isl = topology.islsForActiveSwitches.find {
             it.aswitch
         } ?: assumeTrue("Wasn't able to find suitable ISL", false)
+        def reverseIsl = islUtils.reverseIsl(isl)
+
+        int islCost = islUtils.getIslCost(isl)
+        assert islUtils.getIslCost(reverseIsl) == islCost
 
         when: "Remove a-switch rules to break link between switches"
-        int islCost = islUtils.getIslCost(isl)
         def rulesToRemove = [new ASwitchFlow(isl.aswitch.inPort, isl.aswitch.outPort),
                              new ASwitchFlow(isl.aswitch.outPort, isl.aswitch.inPort)]
-
         lockKeeper.removeFlows(rulesToRemove)
 
-        then: "ISL status becomes 'FAILED'"
+        then: "Status of forward and reverse ISLs becomes 'FAILED'"
         Wrappers.wait(discoveryTimeout * 1.5 + WAIT_OFFSET) {
-            assert islUtils.getIslInfo(isl).get().state == IslChangeType.FAILED
-            assert islUtils.getIslInfo(islUtils.reverseIsl(isl)).get().state == IslChangeType.FAILED
+            def links = northbound.getAllLinks()
+            assert islUtils.getIslInfo(links, isl).get().state == IslChangeType.FAILED
+            assert islUtils.getIslInfo(links, reverseIsl).get().state == IslChangeType.FAILED
         }
 
-        and: "ISL cost after connection loss is not increased"
+        and: "Cost of forward and reverse ISLs after failing connection is not increased"
         islUtils.getIslCost(isl) == islCost
-        islUtils.getIslCost(islUtils.reverseIsl(isl)) == islCost
+        islUtils.getIslCost(reverseIsl) == islCost
 
         and: "Add a-switch rules to restore connection"
         lockKeeper.addFlows(rulesToRemove)
         Wrappers.wait(discoveryInterval + WAIT_OFFSET) {
-            assert islUtils.getIslInfo(isl).get().state == IslChangeType.DISCOVERED
+            def links = northbound.getAllLinks()
+            assert islUtils.getIslInfo(links, isl).get().state == IslChangeType.DISCOVERED
+            assert islUtils.getIslInfo(links, reverseIsl).get().state == IslChangeType.DISCOVERED
         }
     }
 }
