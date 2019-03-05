8f7afeb8b89d121ac5172633b52fe4c88e847a12
Hitesh Gupta <hitesh.gupta@cloudsmartz.net>
2019-03-05 14:51:10 +0530

    OpenTSDB metric prefix changes for UI

--- services/src/openkilda-gui/ui/src/app/common/services/dygraph.service.ts
+++ services/src/openkilda-gui/ui/src/app/common/services/dygraph.service.ts
@@ -333,7 +333,8 @@ export class DygraphService {
         for (let j = 0; j < data.length; j++) {
           var dataValues = typeof data[j] !== "undefined" ? data[j].dps : null;
           var metric = typeof data[j] !== "undefined" ? data[j].metric : "";
-          if (metric !== "pen.flow.packets") {
+          var tempMetric = metric.split('.').slice(1).join('.') /**remove metric prefix */
+          if(tempMetric !== "flow.packets"){
             metric = metric + "(switchid=" + data[j].tags.switchid + ", cookie="+data[j].tags['cookie']+")";
             labels.push(metric);
             var colorCode = this.getColorCode(j, color);
