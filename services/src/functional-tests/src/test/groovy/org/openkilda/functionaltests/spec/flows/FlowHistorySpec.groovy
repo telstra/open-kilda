4d41f4b6f002336f8e80148c825155909079b7aa
Andrii Dovhan <dovhan.andriy@gmail.com>
2019-03-06 13:17:17 +0200

    IMPROVE: refactor existed history tests

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/FlowHistorySpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/FlowHistorySpec.groovy
@@ -6,6 +6,7 @@ import org.openkilda.functionaltests.BaseSpecification
 import org.openkilda.testing.model.topology.TopologyDefinition.Switch
 
 import spock.lang.Narrative
+import spock.lang.Shared
 
 @Narrative("""Verify that history records are created for the create/update actions.
 History record is created in case the create/update action is completed successfully.""")
@@ -15,51 +16,41 @@ class FlowHistorySpec extends BaseSpecification {
     String updateAction = "Flow updating"
     String updateHistoryAction = "Updated the flow"
 
-    def "History records are created for the create/update actions using custom timeline"() {
-        given: "Current timestamp"
-        Long timestampBefore = (System.currentTimeMillis() / 1000L) - 1000
+    @Shared
+    Long timestampBefore
 
+    def setupOnce() {
+        timestampBefore = System.currentTimeSeconds() - 5
+    }
+
+    def "History records are created for the create/update actions using custom timeline"() {
         when: "Create a flow"
         def (Switch srcSwitch, Switch dstSwitch) = topology.activeSwitches
         def flow = flowHelper.randomFlow(srcSwitch, dstSwitch)
         flowHelper.addFlow(flow)
 
         then: "History record is created"
-        Long timestampAfterCreate = System.currentTimeMillis() / 1000L
-        def flowH1 = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterCreate)
-        flowH1.size() == 1
-        flowH1[0].flowId == flow.id
-        flowH1[0].action == createAction
-        flowH1[0].taskId
-        flowH1[0].histories.action[0] == createHistoryAction
+        Long timestampAfterCreate = System.currentTimeSeconds()
+        def flowHistory = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterCreate)
+        assert flowHistory.size() == 1
+        checkHistoryCreateAction(flowHistory[0], flow.id)
 
         when: "Update the created flow"
         flowHelper.updateFlow(flow.id, flow.tap { it.description = it.description + "updated" })
 
         then: "History record is created after updating the flow"
-        Long timestampAfterUpdate = System.currentTimeMillis() / 1000L
-        def flowH2 = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterUpdate)
-        flowH2.size() == 2
-        flowH2[1].flowId == flow.id
-        flowH2[1].action == updateAction
-        flowH2[1].taskId
-        flowH2[1].histories.action[0] == updateHistoryAction
+        Long timestampAfterUpdate = System.currentTimeSeconds()
+        def flowHistory1 = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterUpdate)
+        assert flowHistory1.size() == 2
+        checkHistoryUpdateAction(flowHistory1[1], flow.id)
 
         when: "Delete the updated flow"
         flowHelper.deleteFlow(flow.id)
 
         then: "History is still available for the deleted flow"
-        def flowH3 = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterUpdate)
-        flowH3.size() == 2
-        flowH3[0].flowId == flow.id
-        flowH3[0].action == createAction
-        flowH3[0].taskId
-        flowH3[0].histories.action[0] == createHistoryAction
-
-        flowH3[1].flowId == flow.id
-        flowH3[1].action == updateAction
-        flowH3[1].taskId
-        flowH3[1].histories.action[0] == updateHistoryAction
+        def flowHistory3 = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterUpdate)
+        assert flowHistory3.size() == 2
+        checkHistoryDeleteAction(flowHistory3, flow.id)
     }
 
     def "History records are created for the create/update actions using default timeline"() {
@@ -69,76 +60,75 @@ class FlowHistorySpec extends BaseSpecification {
         flowHelper.addFlow(flow)
 
         then: "History record is created"
-        def flowH1 = northbound.getFlowHistory(flow.id)
-
-        flowH1.size() == 1
-        flowH1[0].flowId == flow.id
-        flowH1[0].action == createAction
-        flowH1[0].taskId
-        flowH1[0].histories.action[0] == createHistoryAction
+        def flowHistory = northbound.getFlowHistory(flow.id)
+        assert flowHistory.size() == 1
+        checkHistoryCreateAction(flowHistory[0], flow.id)
 
         when: "Update the created flow"
         flowHelper.updateFlow(flow.id, flow.tap { it.description = it.description + "updated" })
 
         then: "History record is created after updating the flow"
-        def flowH2 = northbound.getFlowHistory(flow.id)
-        flowH2.size() == 2
-        flowH2[1].flowId == flow.id
-        flowH2[1].action == updateAction
-        flowH2[1].taskId
-        flowH2[1].histories.action[0] == updateHistoryAction
+        def flowHistory1 = northbound.getFlowHistory(flow.id)
+        assert flowHistory1.size() == 2
+        checkHistoryUpdateAction(flowHistory1[1], flow.id)
 
         when: "Delete the updated flow"
         flowHelper.deleteFlow(flow.id)
 
         then: "History is still available for the deleted flow"
-        def flowH3 = northbound.getFlowHistory(flow.id)
-        flowH3.size() == 2
-        flowH3[0].flowId == flow.id
-        flowH3[0].action == createAction
-        flowH3[0].taskId
-        flowH3[0].histories.action[0] == createHistoryAction
-
-        flowH3[1].flowId == flow.id
-        flowH3[1].action == updateAction
-        flowH3[1].taskId
-        flowH3[1].histories.action[0] == updateHistoryAction
+        def flowHistory3 = northbound.getFlowHistory(flow.id)
+        assert flowHistory3.size() == 2
+        checkHistoryDeleteAction(flowHistory3, flow.id)
     }
 
     def "History should not be returned in case timeline is incorrect (timeBefore > timeAfter)"() {
-        given: "Current timestamp"
-        Long timestampBefore = (System.currentTimeMillis() / 1000L) - 1000
-
         when: "Create a flow"
         def (Switch srcSwitch, Switch dstSwitch) = topology.activeSwitches
         def flow = flowHelper.randomFlow(srcSwitch, dstSwitch)
         flowHelper.addFlow(flow)
 
         then: "History record is created"
-        Long timestampAfterCreate = System.currentTimeMillis() / 1000L
-        def flowH1 = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterCreate)
-
-        flowH1.size() == 1
-        flowH1[0].flowId == flow.id
-        flowH1[0].action == createAction
-        flowH1[0].taskId
-        flowH1[0].histories.action[0] == createHistoryAction
+        Long timestampAfterCreate = System.currentTimeSeconds()
+        def flowHistory = northbound.getFlowHistory(flow.id, timestampBefore, timestampAfterCreate)
+        assert flowHistory.size() == 1
+        checkHistoryCreateAction(flowHistory[0], flow.id)
 
         when: "Try to get history for incorrect timeline"
-        def flowH2 = northbound.getFlowHistory(flow.id, timestampAfterCreate, timestampBefore)
+        def flowH = northbound.getFlowHistory(flow.id, timestampAfterCreate, timestampBefore)
 
         then: "History record is NOT returned"
-        flowH2.isEmpty()
+        flowH.isEmpty()
 
-        and: "cleanup: Restore default state(remove created flow)"
+        and: "Cleanup: restore default state(remove created flow)"
         flowHelper.deleteFlow(flow.id)
     }
 
-    def "History should not be returned in case flowId was never created"() {
+    def "History should not be returned in case flow was never created"() {
         when: "Try to get history for incorrect flowId"
         def flowHistory = northbound.getFlowHistory(NON_EXISTENT_FLOW_ID)
 
         then: "History record is NOT returned"
         flowHistory.isEmpty()
+    }
+
+    void checkHistoryCreateAction(flowHistory, flowId) {
+        assert flowHistory.flowId == flowId
+        assert flowHistory.action == createAction
+        assert flowHistory.taskId
+        assert flowHistory.histories.action[0] == createHistoryAction
+    }
+
+    void checkHistoryUpdateAction(flowHistory, flowId) {
+        assert flowHistory.flowId == flowId
+        assert flowHistory.action == updateAction
+        assert flowHistory.taskId
+        assert flowHistory.histories.action[0] == updateHistoryAction
+    }
+
+    /** We pass latest timestamp when changes were done.
+     * Just for getting all records from history */
+    void checkHistoryDeleteAction(flowHistory, flowId) {
+        checkHistoryCreateAction(flowHistory[0], flowId)
+        checkHistoryUpdateAction(flowHistory[1], flowId)
     }
 }
