31cdd080dbeaf01f5c2c5cddd6efa9cd40b2462b
Ilya Moiseev <freaxz@gmail.com>
2019-04-07 15:28:49 +0400

    Review fixes

--- services/wfm/src/main/java/org/openkilda/wfm/topology/switchmanager/service/impl/ValidationServiceImpl.java
+++ services/wfm/src/main/java/org/openkilda/wfm/topology/switchmanager/service/impl/ValidationServiceImpl.java
@@ -59,7 +59,7 @@ public class ValidationServiceImpl implements ValidationService {
                 .map(Cookie::getValue)
                 .collect(Collectors.toSet());
 
-        flowPathRepository.findByEndpointSwitchForRules(switchId).stream()
+        flowPathRepository.findByEndpointSwitch(switchId).stream()
                 .map(FlowPath::getCookie)
                 .map(Cookie::getValue)
                 .forEach(expectedCookies::add);
