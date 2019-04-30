3d92442c04e28c34adfdc0b0b8c703f72b8e3588
Sergey Nikitin <snikitin@mirantis.com>
2019-04-30 17:50:48 +0400

    Refactor of PathVerificationService
    Part of #580

--- services/src/floodlight-service/floodlight-modules/src/test/java/org/openkilda/floodlight/pathverification/PathVerificationPacketSignTest.java
+++ services/src/floodlight-service/floodlight-modules/src/test/java/org/openkilda/floodlight/pathverification/PathVerificationPacketSignTest.java
@@ -16,17 +16,13 @@
 package org.openkilda.floodlight.pathverification;
 
 import static org.easymock.EasyMock.anyObject;
-import static org.easymock.EasyMock.capture;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.expectLastCall;
-import static org.easymock.EasyMock.newCapture;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
 import org.openkilda.floodlight.model.OfInput;
 import org.openkilda.messaging.Message;
-import org.openkilda.messaging.info.InfoMessage;
-import org.openkilda.messaging.info.event.IslInfoData;
 
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
@@ -34,8 +30,6 @@ import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPacket;
-import net.floodlightcontroller.packet.PacketParsingException;
-import org.easymock.Capture;
 import org.easymock.EasyMock;
 import org.easymock.EasyMockRunner;
 import org.easymock.IAnswer;
@@ -117,29 +111,7 @@ public class PathVerificationPacketSignTest extends PathVerificationPacketInTest
     }
 
     @Test
-    public void testHandlePacketInWithDifferentPortSpeeds() {
-        Capture<InfoMessage> capturedArgument = newCapture();
-
-        producerService.sendMessageAndTrack(anyObject(), anyObject(), capture(capturedArgument));
-        expectLastCall().once();
-        replay(producerService);
-
-        pvs.handlePacketIn(new OfInput(sw2, ofPacketIn, context));
-        verify(producerService);
-
-        IslInfoData data = (IslInfoData) capturedArgument.getValue().getData();
-
-        long firstSpeed = sw1.getPort(OFPort.of(1)).getCurrSpeed();
-        long secondSpeed = sw2.getPort(OFPort.of(1)).getCurrSpeed();
-
-        // checks that port speeds are different (otherwise test is useless)
-        Assert.assertNotEquals(firstSpeed, secondSpeed);
-        // checks that minimal port speed will be used
-        Assert.assertEquals(Math.min(firstSpeed, secondSpeed), data.getSpeed());
-    }
-
-    @Test
-    public void testSignPacketMissedSign() throws PacketParsingException, FloodlightModuleException {
+    public void testSignPacketMissedSign() {
         replay(producerService);
 
         OFPacketOut noSignPacket = pvs.generateVerificationPacket(sw1, OFPort.of(1), false, null);
@@ -153,7 +125,7 @@ public class PathVerificationPacketSignTest extends PathVerificationPacketInTest
     }
 
     @Test
-    public void testSignPacketInvalidSign() throws PacketParsingException, FloodlightModuleException {
+    public void testSignPacketInvalidSign() throws FloodlightModuleException {
         replay(producerService);
 
         pvs.initAlgorithm("secret2");
