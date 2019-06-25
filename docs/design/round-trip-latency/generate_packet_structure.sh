e586d7e60539722947bad877c1766e2055e81d4c
Sergey Nikitin <snikitin@mirantis.com>
2019-06-25 15:17:47 +0400

    Design for Round Trup Latency
    Related To #580

---
+++ docs/design/round-trip-latency/generate_packet_structure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+structure="Destination Mac Address:48,Source Mac Address:48,EtherType:16,Version:4,\
+IHL:4,Type of Service:8,Total Length:16,Identification:16,Flags:3,Fragment Offset:13,\
+Time to Live:8,Protocol:8,Header Checksum:16,Source IP Address:32,Destination IP Address:32,\
+Source Port:16,Destination Port:16,Length:16,Checksum:16,ChasisID Type:7,length:9,\
+Chassis Id:56,Port Id Type:7,length:9,Port:24,TTL Type:7,length:9,TTL:16,Custom Type:7,\
+length:9,Organizationally Unique Identifier:24,Timestamp type:8,Timestamp T0:64,Custom Type:7,\
+length:9,Organizationally Unique Identifier:24,Timestamp type:8,Timestamp T1:64,Custom Type:7,\
+length:9,Organizationally Unique Identifier:24,SwitchId type:8,Datapath ID:64,Custom Type:7,\
+length:9,Organizationally Unique Identifier:24,Timestamp type:8,Floodlight Timestamp:64,\
+Custom Type:7,length:9,Organizationally Unique Identifier:24,Ordinal type:8,Ordinal:32,\
+Custom Type:7,length:9,Organizationally Unique Identifier:24,Sigh type:8,Token:131,End of LLDP:16"
+
+protocol "$structure" --bits 48
