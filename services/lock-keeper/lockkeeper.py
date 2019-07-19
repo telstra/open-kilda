cc3c152b5dba7b80b68c527bfddf8c73a1f5f260
Andrii Dovhan <dovhan.andriy@gmail.com>
2019-07-19 17:12:57 +0300

    add new endpoint for managing iptables rules on floodlight

--- services/lock-keeper/lockkeeper.py
+++ services/lock-keeper/lockkeeper.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 HOST = os.environ.get("LOCK_KEEPER_HOST")
 USER = os.environ.get("LOCK_KEEPER_USER")
 SECRET = os.environ.get("LOCK_KEEPER_SECRET")
-FL_CONTAINER_NAME = "floodlight"
+FL_CONTAINER_NAME = "floodlight_mgmt_1"
 PORT = 22
 
 
@@ -25,6 +25,12 @@ def execute_remote_commands(commands):
         data.append((stdout.read() + stderr.read()).decode('utf-8'))
     client.close()
     return data
+
+
+def execute_command_in_container(commands, container_name=FL_CONTAINER_NAME):
+    for command in commands:
+        docker.from_env().containers.get(container_name).exec_run(command)
+    return True
 
 
 def execute_remote_command(command):
@@ -123,4 +129,40 @@ def fl_start():
 @app.route('/floodlight/restart', methods=['POST'])
 def fl_restart():
     docker.from_env().containers.get(FL_CONTAINER_NAME).restart()
+    return jsonify({'status': 'ok'})
+
+
+@app.route('/block-access', methods=['POST'])
+def block_access():
+    body = request.get_json()
+    commands = []
+    if 'ip' in body:
+        commands.append('iptables -I INPUT -s {} -j DROP'.format(body['ip']))
+        commands.append('iptables -I OUTPUT -s {} -j DROP'.format(body['ip']))
+    else:
+        commands.append('iptables -I INPUT -p tcp --source-port {} -j DROP'.format(body['port']))
+        commands.append('iptables -I OUTPUT -p tcp --destination-port {} -j DROP'.format(body['port']))
+
+    execute_command_in_container(commands)
+    return jsonify({'status': 'ok'})
+
+
+@app.route('/unblock-access', methods=['POST'])
+def unblock_access():
+    body = request.get_json()
+    commands = []
+    if 'ip' in body:
+        commands.append('iptables -D INPUT -s {} -j DROP'.format(body['ip']))
+        commands.append('iptables -D OUTPUT -s {} -j DROP'.format(body['ip']))
+    else:
+        commands.append('iptables -D INPUT -p tcp --source-port {} -j DROP'.format(body['port']))
+        commands.append('iptables -D OUTPUT -p tcp --destination-port {} -j DROP'.format(body['port']))
+
+    execute_command_in_container(commands)
+    return jsonify({'status': 'ok'})
+
+
+@app.route('/allow-access-to-everything', methods=['POST'])
+def allow_access_to_everything():
+    execute_command_in_container(['iptables -F INPUT', 'iptables -F OUTPUT'])
     return jsonify({'status': 'ok'})
