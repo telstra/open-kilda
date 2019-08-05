74832013ae406f70d1e0295e9b9d6d44c2f7636f
Dmitry Bogun <nyaka@nyaka.org>
2019-08-05 20:26:51 +0300

    Produce LLDP packets with traffexam
    New REST endpoint aimed to push LLDP packets via some existing
    interface(so it can be VLAN tagged if interface have VLAN
    assignment).
    
    Endpoint definition:
    
    PUT /address/<idnr>/lldp
    
    {
      "mac_address": "02:00:00:00:00:01",
      "port_number": "5",
      "time_to_live": 60
    }
    
    Response body:
    {
      "lldp_push": {
        "sent_packets": 1
      }
    }
    
    Be careful with mac addresse field - avoid 1 in less significant bit of
    first byte (mac address start with any one of this - x1:, x3:, x5:, x7:,
    x9:, xb: , xd: , xf:, where 'x' is any hex characted). This bit identify
    "group/broadcast" recipient and filtered out on any bridge. And because
    traffexam are attached to target interface via linux bridge, such
    packets will never reach recipient.

---
+++ services/lab-service/traffexam/kilda/traffexam/action.py
@@ -0,0 +1,61 @@
+# Copyright 2017 Telstra Open Source
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+import functools
+import pathlib
+import operator
+
+import nsenter
+from scapy.all import Ether, sendp
+from scapy.contrib import lldp
+
+from kilda.traffexam import context as context_module
+
+
+class Abstract(context_module.ContextConsumer):
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class LLDPPush(Abstract):
+    ns_fs_location = pathlib.Path('/var/run/netns')
+    lldp_bcast_mac = '01:80:c2:00:00:0e'
+
+    def __call__(self, iface, push_entry):
+        pkt = self._encode(push_entry)
+
+        ns_name = self.context.make_network_namespace_name()
+        ns_fd = self.ns_fs_location / ns_name
+        with nsenter.Namespace(str(ns_fd), 'net'):
+            sendp(pkt, iface=iface.name, verbose=False, promisc=False)
+
+    def _encode(self, entry):
+        entries = (
+            lldp.LLDPDUChassisID(
+                subtype=lldp.LLDPDUChassisID.SUBTYPE_MAC_ADDRESS,
+                id=entry.mac_address),
+            lldp.LLDPDUPortID(
+                subtype=lldp.LLDPDUPortID.SUBTYPE_LOCALLY_ASSIGNED,
+                id=entry.port_number),
+            lldp.LLDPDUTimeToLive(ttl=entry.time_to_live),
+            lldp.LLDPDUEndOfLLDPDU())
+        payload = functools.reduce(operator.truediv, entries)
+
+        return Ether(src=entry.mac_address, dst=self.lldp_bcast_mac) / payload
+
+
+class Adapter(object):
+    def __init__(self, context):
+        self.lldp_push = LLDPPush(context)
