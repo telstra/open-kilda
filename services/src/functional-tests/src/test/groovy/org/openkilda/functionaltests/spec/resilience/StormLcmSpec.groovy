c49fd623f01926a926c8d13f4a8fc76cfd136d65
Andrii Dovhan <dovhan.andriy@gmail.com>
2019-08-22 17:10:09 +0300

    fix stormLcmSpec

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/resilience/StormLcmSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/resilience/StormLcmSpec.groovy
@@ -53,16 +53,19 @@ class StormLcmSpec extends HealthCheckSpecification {
         }
 
         and: "Database dump"
-        def nodesDump = database.dumpAllNodes()
         def relationsDump = database.dumpAllRelations()
+        def switchesDump = database.dumpAllSwitches()
+        def islsDump = database.dumpAllIsls()
 
         when: "Storm topologies are restarted"
         wfmManipulator.restartWfm()
 
         then: "Database nodes and relations are unchanged"
-        def newNodes = database.dumpAllNodes()
         def newRelation = database.dumpAllRelations()
-        expect newNodes, sameBeanAs(nodesDump)
+        def newSwitches = database.dumpAllSwitches()
+        def newIsls = database.dumpAllIsls()
+        expect newSwitches, sameBeanAs(switchesDump)
+        expect newIsls, sameBeanAs(islsDump)
         expect newRelation, sameBeanAs(relationsDump).ignoring("time_modify").ignoring("latency")
 
         and: "Flows remain valid in terms of installed rules and meters"
