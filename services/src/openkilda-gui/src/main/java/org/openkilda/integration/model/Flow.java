594c00d04ff3d1ecb5e69bc831b1006b6363efdd
Swati Sharma <swati.sharma@cloudsmartz.net>
2019-10-24 15:09:13 +0530

    #Checkstyle fixes

--- services/src/openkilda-gui/src/main/java/org/openkilda/integration/model/Flow.java
+++ services/src/openkilda-gui/src/main/java/org/openkilda/integration/model/Flow.java
@@ -15,14 +15,14 @@
 
 package org.openkilda.integration.model;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import lombok.Data;
+
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
