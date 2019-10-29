90534ddef088d80cbf6c9274f3bea2c7bac508a2
Neeraj <neeraj.kumar@cloudsmartz.net>
2019-10-29 16:01:48 +0530

    - fixed issue with network path page reload

--- services/src/openkilda-gui/src/main/java/org/openkilda/controller/CommonRequestHandlerController.java
+++ services/src/openkilda-gui/src/main/java/org/openkilda/controller/CommonRequestHandlerController.java
@@ -24,7 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping(value = { "/home", "/topology", "/flows", "/isl", "/switches", "/usermanagement", "/useractivity",
-        "/storesetting", "/application-setting" }, method = RequestMethod.GET)
+        "/storesetting", "/application-setting", "/networkpath" }, method = RequestMethod.GET)
 public class CommonRequestHandlerController extends BaseController {
     /**
      * CommonRequestHandlerController.
