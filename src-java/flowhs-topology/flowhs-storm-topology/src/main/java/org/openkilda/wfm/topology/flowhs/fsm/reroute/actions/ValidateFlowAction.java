bebdb13d2dbe69c31a90932030aaff1c520eb554
Timofey Durakov <timofei.nd@gmail.com>
2020-01-09 10:43:07 +0400

    Added checks for reroute command which verify that endpoint switches are active.

--- src-java/flowhs-topology/flowhs-storm-topology/src/main/java/org/openkilda/wfm/topology/flowhs/fsm/reroute/actions/ValidateFlowAction.java
+++ src-java/flowhs-topology/flowhs-storm-topology/src/main/java/org/openkilda/wfm/topology/flowhs/fsm/reroute/actions/ValidateFlowAction.java
@@ -77,6 +77,15 @@ public class ValidateFlowAction extends NbTrackableAction<FlowRerouteFsm, State,
                         format("Flow %s is in progress now", flowId));
             }
 
+            if (!foundFlow.getSrcSwitch().isActive()) {
+                throw new FlowProcessingException(ErrorType.UNPROCESSABLE_REQUEST,
+                        format("Flow's %s src switch is not active", flowId));
+            }
+            if (!foundFlow.getDestSwitch().isActive()) {
+                throw new FlowProcessingException(ErrorType.UNPROCESSABLE_REQUEST,
+                        format("Flow's %s dest switch is not active", flowId));
+            }
+
             stateMachine.setOriginalFlowStatus(foundFlow.getStatus());
             stateMachine.setOriginalEncapsulationType(foundFlow.getEncapsulationType());
             stateMachine.setOriginalPathComputationStrategy(foundFlow.getPathComputationStrategy());
