4fa7cc1514d5d3ecafce36fae29f154fdcff140e
AndriiDovhan <andriidovhan@users.noreply.github.com>
2020-02-04 12:13:43 +0200

    minor improvements in featureToogle spec (#3116)

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/toggles/FeatureTogglesSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/toggles/FeatureTogglesSpec.groovy
@@ -20,11 +20,9 @@ creation of new flows via Northbound API. This spec verifies that Feature Toggle
 /*Note that the 'flowReroute' toggle is tested under AutoRerouteSpec#"Flow goes to 'Down' status when an intermediate
 switch is disconnected and there is no ability to reroute".
 BFD toggle is tested in BfdSpec*/
-@Tags([SMOKE, LOW_PRIORITY])
+//TODO (andriidovhan) remove VIRTUAL tag when mapping is disabled
+@Tags([SMOKE, LOW_PRIORITY, VIRTUAL])
 class FeatureTogglesSpec extends HealthCheckSpecification {
-    @Tags(VIRTUAL)
-    //TODO (andriidovhan) remove VIRTUAL tag and add new message when Issue 2797 is fixed
-    // new error message in APIv2 "Could not create flow: Flow create feature is disabled"
     def "System forbids creating new flows when 'create_flow' toggle is set to false"() {
         given: "Existing flow"
         def flow = flowHelper.randomFlow(topology.activeSwitches[0], topology.activeSwitches[1])
