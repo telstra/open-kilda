0da871e5b812e7a778f4f62125133a40d96999db
Andrii Dovhan <dovhan.andriy@gmail.com>
2020-02-19 12:12:07 +0200

    improve MultitableFlowsSpec

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/multitable/MultitableFlowsSpec.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/multitable/MultitableFlowsSpec.groovy
@@ -365,11 +365,12 @@ mode with existing flows and hold flows of different table-mode types"() {
         List<PathNode> desiredPath = null
         List<Switch> involvedSwitches = null
         def switchPair = topologyHelper.allNotNeighboringSwitchPairs.collectMany { [it, it.reversed] }.find { pair ->
-            desiredPath = pair.paths.find { path ->
-                involvedSwitches = pathHelper.getInvolvedSwitches(path)
-                involvedSwitches.size() == 3 &&
-                        involvedSwitches.every { it.features.contains(SwitchFeature.MULTI_TABLE) }
+            def allPaths = pair.paths.findAll { path ->
+                pathHelper.getInvolvedSwitches(path).every { it.features.contains(SwitchFeature.MULTI_TABLE) }
             }
+            desiredPath = allPaths.find { pathHelper.getInvolvedSwitches(it).size() == 3 }
+            // make sure that alternative path for protected path is available
+            allPaths.findAll { it.intersect(desiredPath) == [] ? 1 : 0 }.size() > 0
         }
         assumeTrue("Unable to find a path with three switches", switchPair.asBoolean())
         //make required path the most preferred
