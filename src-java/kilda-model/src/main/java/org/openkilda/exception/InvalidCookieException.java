telstra/Kilda

Toolset for cookie bit manipulations
Decouple different kind of cookies biit layout intto different
tools(schemas).

src-java/kilda-model/src/main/java/org/openkilda/exception/InvalidCookieException.java
@@ -1,0 +1,30 @@
+/* Copyright 2020 Telstra Open Source
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.openkilda.exception;
+
+import org.openkilda.model.Cookie;
+
+import lombok.Getter;
+
+public class InvalidCookieException extends Exception {
+    @Getter
+    private final Cookie cookie;
+
+    public InvalidCookieException(String message, Cookie cookie) {
+        super(String.format("%s (%s)", message, cookie));
+        this.cookie = cookie;
+    }
+}

telstra/open-kilda

src-java/kilda-model/src/main/java/org/openkilda/exception/InvalidCookieException.java
@@ -1,0 +1,30 @@
+/* Copyright 2020 Telstra Open Source
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.openkilda.exception;
+
+import org.openkilda.model.CookieBase;
+
+import lombok.Getter;
+
+public class InvalidCookieException extends Exception {
+    @Getter
+    private final CookieBase cookie;
+
+    public InvalidCookieException(String message, CookieBase cookie) {
+        super(String.format("%s (%s)", message, cookie));
+        this.cookie = cookie;
+    }
+}