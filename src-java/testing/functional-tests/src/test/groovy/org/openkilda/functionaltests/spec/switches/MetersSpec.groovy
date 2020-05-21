78ff261d4a1245188b6581d65abd17ebc4ba7766
Andrii Dovhan <dovhan.andriy@gmail.com>
2020-05-21 17:26:51 +0300

    fix MetersSpec.groovy

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/switches/MetersSpec.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/switches/MetersSpec.groovy
@@ -27,6 +27,8 @@ import org.openkilda.messaging.info.meter.MeterEntry
 import org.openkilda.messaging.info.rule.FlowEntry
 import org.openkilda.messaging.info.rule.SwitchFlowEntries
 import org.openkilda.model.SwitchId
+import org.openkilda.model.cookie.Cookie
+import org.openkilda.model.cookie.CookieBase.CookieType
 import org.openkilda.testing.Constants
 import org.openkilda.testing.model.topology.TopologyDefinition.Switch
 
@@ -317,14 +319,43 @@ meters in flow rules at all (#data.flowType flow)"() {
         srcSwFlowMeters.size() == 1
         dstSwFlowMeters.size() == 1
 
-        def srcSwitchRules = northbound.getSwitchRules(flow.source.switchId).flowEntries
-        def dstSwitchRules = northbound.getSwitchRules(flow.destination.switchId).flowEntries
-        def srcSwFlowIngressRule = filterRules(srcSwitchRules, flow.source.portNumber, flow.source.vlanId, null)[0]
-        def dstSwFlowIngressRule = filterRules(dstSwitchRules, flow.destination.portNumber, flow.destination.vlanId,
-                null)[0]
+        def srcSwitchRules = northbound.getSwitchRules(flow.source.switchId).flowEntries.findAll {  !Cookie.isDefaultRule(it.cookie) }
+        def dstSwitchRules = northbound.getSwitchRules(flow.destination.switchId).flowEntries.findAll {  !Cookie.isDefaultRule(it.cookie) }
 
-        srcSwFlowMeters[0].meterId == srcSwFlowIngressRule.instructions.goToMeter
-        dstSwFlowMeters[0].meterId == dstSwFlowIngressRule.instructions.goToMeter
+        if (northbound.getSwitchProperties(flow.source.switchId).multiTable) {
+            def srcSwIngressFlowRules = srcSwitchRules.findAll { it.match.inPort == flow.source.portNumber.toString() }
+            assert srcSwIngressFlowRules.size() == 2 //shared + simple ingress
+            def srcSwIngressSharedRule = srcSwIngressFlowRules.find {
+                new Cookie(it.cookie).getType() == CookieType.SHARED_OF_FLOW
+            }
+            def srcSwIngressNonSharedRule = srcSwIngressFlowRules.find {
+                new Cookie(it.cookie).getType() != CookieType.SHARED_OF_FLOW
+            }
+            assert srcSwIngressSharedRule.match.vlanVid == flow.source.vlanId.toString()
+            assert !srcSwIngressSharedRule.instructions.goToMeter
+            assert srcSwFlowMeters[0].meterId == srcSwIngressNonSharedRule.instructions.goToMeter
+        } else {
+            def srcSwFlowIngressRule = filterRules(srcSwitchRules, flow.source.portNumber, flow.source.vlanId, null)[0]
+            srcSwFlowMeters[0].meterId == srcSwFlowIngressRule.instructions.goToMeter
+        }
+
+        if (northbound.getSwitchProperties(flow.destination.switchId).multiTable) {
+            def dstSwIngressFlowRules = dstSwitchRules.findAll { it.match.inPort == flow.destination.portNumber.toString() }
+            assert dstSwIngressFlowRules.size() == 2 //shared + simple ingress
+            def dstSwIngressSharedRule = dstSwIngressFlowRules.find {
+                new Cookie(it.cookie).getType() == CookieType.SHARED_OF_FLOW
+            }
+            def dstSwIngressNonSharedRule = dstSwIngressFlowRules.find {
+                new Cookie(it.cookie).getType() != CookieType.SHARED_OF_FLOW
+            }
+            assert dstSwIngressSharedRule.match.vlanVid == flow.destination.vlanId.toString()
+            assert !dstSwIngressSharedRule.instructions.goToMeter
+            assert dstSwFlowMeters[0].meterId == dstSwIngressNonSharedRule.instructions.goToMeter
+        } else {
+            def dstSwFlowIngressRule = filterRules(dstSwitchRules, flow.destination.portNumber, flow.destination.vlanId,
+                    null)[0]
+            dstSwFlowMeters[0].meterId == dstSwFlowIngressRule.instructions.goToMeter
+        }
 
         and: "The source and destination switches have no meters in the flow's egress rule"
         def srcSwFlowEgressRule = filterRules(srcSwitchRules, null, null, flow.source.portNumber)[0]
