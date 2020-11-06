4bd5ce200ebfb2441d1252c9abba7541cbac7dda
Andrii Dovhan <dovhan.andriy@gmail.com>
2020-11-06 19:07:28 +0200

    do not merge, test for debigging only

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/MultiRerouteSpec.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/MultiRerouteSpec.groovy
@@ -1,14 +1,20 @@
 package org.openkilda.functionaltests.spec.flows
 
+import static groovyx.gpars.GParsPool.withPool
 import static org.junit.Assume.assumeTrue
+import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.REROUTE_ACTION
+import static org.openkilda.functionaltests.helpers.FlowHistoryConstants.REROUTE_FAIL
 import static org.openkilda.functionaltests.helpers.Wrappers.wait
+import static org.openkilda.messaging.info.event.IslChangeType.FAILED
 import static org.openkilda.testing.Constants.WAIT_OFFSET
 
 import org.openkilda.functionaltests.HealthCheckSpecification
 import org.openkilda.functionaltests.extension.failfast.Tidy
+import org.openkilda.functionaltests.helpers.PathHelper
 import org.openkilda.messaging.info.event.IslChangeType
 import org.openkilda.messaging.payload.flow.FlowState
 import org.openkilda.northbound.dto.v2.flows.FlowRequestV2
+import org.openkilda.testing.model.topology.TopologyDefinition.Isl
 import org.openkilda.testing.tools.SoftAssertions
 
 import spock.lang.Ignore
@@ -101,5 +107,108 @@ class MultiRerouteSpec extends HealthCheckSpecification {
         wait(WAIT_OFFSET + discoveryInterval) {
             assert northbound.getLink(islToBreak).state == IslChangeType.DISCOVERED
         }
+    }
+
+    def "multiReroute for checking perf"() {
+        given: "Switch pair with several available paths, one path should have a transit link \
+        (we will use for a flow2 and break this link) and at least 1 path must remain safe"
+        def allPathsFlow1, allPathCandidatesFlow1, allPathsFlow2
+        def transitLinks
+        def swPair2
+        def flow2Path, flow1Path, flow1BackupPath
+        def swPair1 = topologyHelper.getAllNeighboringSwitchPairs().find { swPFlow1 ->
+                    allPathsFlow1 = swPFlow1.paths
+                    flow1BackupPath = allPathsFlow1.min { it.size() }
+                    allPathCandidatesFlow1 = allPathsFlow1.findAll { pathHelper.getInvolvedSwitches(it).size() >= 4 }
+                    transitLinks = allPathCandidatesFlow1.collectMany { (pathHelper.getInvolvedIsls(it)[1..-2]) }.unique()
+                    swPair2 = topologyHelper.getAllNotNeighboringSwitchPairs().find { swPFlow2 ->
+                        allPathsFlow2 = swPFlow2.paths
+                        def srcSwIdToMatch = swPFlow1.src.dpId
+                        def dstSwIdToAvoid = swPFlow1.dst.dpId
+                        def transitIslsFlow2
+                        flow2Path = allPathsFlow2.find { path ->
+                            if (pathHelper.getInvolvedSwitches(path).size() > 3) {
+                                transitIslsFlow2 = pathHelper.getInvolvedIsls(path)[1..-2]
+                                swPFlow2.src.dpId == srcSwIdToMatch && swPFlow2.dst.dpId != dstSwIdToAvoid && transitIslsFlow2.any { it in transitLinks }
+                            }
+                        }
+                        flow1Path = allPathCandidatesFlow1.find { pathHelper.getInvolvedIsls(it)[1..-2].any {it in transitIslsFlow2} }
+                        flow2Path && flow1Path
+                    }
+                }
+
+        def involvedIslsInMainPaths = [flow1Path, flow2Path, flow1BackupPath].collectMany { pathHelper.getInvolvedIsls(it)}
+                .collectMany {[it, it.reversed] }.unique()
+
+
+        and: "All alternative path are unavailable"
+        def islsToBreak = (allPathsFlow1 + allPathsFlow2).collectMany { pathHelper.getInvolvedIsls(it) }
+                .collectMany { [it, it.reversed] }.unique()
+                .findAll { !involvedIslsInMainPaths.contains(it) }.unique { [it, it.reversed].sort() }
+        withPool { islsToBreak.eachParallel { Isl isl -> antiflap.portDown(isl.srcSwitch.dpId, isl.srcPort) } }
+        wait(WAIT_OFFSET) {
+            assert northbound.getAllLinks().findAll { it.state == FAILED }.size() == islsToBreak.size() * 2
+        }
+
+        and: "flow1MainPath is more preferable than flow1BackupPath"
+        pathHelper.makePathNotPreferable(flow1BackupPath)
+
+        and: "Flows on the given switch pairs and paths"
+        List<FlowRequestV2> flows1 = []
+        20.times {
+            def flow = flowHelperV2.randomFlow(swPair1, false, flows1)
+            flowHelperV2.addFlow(flow)
+            flows1 << flow
+        }
+        List<FlowRequestV2> flows2 = []
+        20.times {
+            def flow = flowHelperV2.randomFlow(swPair2, false, flows1 + flows2)
+            flowHelperV2.addFlow(flow)
+            flows2 << flow
+        }
+        flows1.each {
+            assert flow1Path == PathHelper.convert(northbound.getFlowPath(it.flowId))
+        }
+        flows2.each {
+            assert flow2Path == PathHelper.convert(northbound.getFlowPath(it.flowId))
+        }
+
+        when: "Init auto reroute, break all transit ISLs for flow1"
+        def transitIslsToFail = pathHelper.getInvolvedIsls(flow1Path)[1..-1]
+        withPool { transitIslsToFail.eachParallel { Isl isl -> antiflap.portDown(isl.srcSwitch.dpId, isl.srcPort) } }
+        wait(WAIT_OFFSET) {
+            assert northbound.getAllLinks().findAll {
+                it.state == FAILED
+            }.size() == (islsToBreak.size() + transitIslsToFail.size()) * 2
+        }
+
+        then: "Flows on flow1MainPath are rerouted to flow1BackupPath"
+        wait(WAIT_OFFSET * 2) {
+        flows1.each {
+                assert flow1BackupPath == PathHelper.convert(northbound.getFlowPath(it.flowId))
+                assert northbound.getFlowStatus(it.flowId).status == FlowState.UP
+            }
+        }
+
+        and: "Flows on flow2Path are NOT rerouted"
+        wait(rerouteDelay + WAIT_OFFSET) {
+            flows2.each {
+                assert northbound.getFlowStatus(it.flowId).status == FlowState.DOWN
+                assert northbound.getFlowHistory(it.flowId).find {
+                    it.action == REROUTE_ACTION && it.taskId =~ (/.+ : retry #1 ignore_bw true/)
+                }?.payload?.last()?.action == REROUTE_FAIL
+            }
+        }
+
+        cleanup:
+        flows1 && flows1.each { flowHelperV2.deleteFlow(it.flowId) }
+        flows2 && flows2.each { flowHelperV2.deleteFlow(it.flowId) }
+        islsToBreak && withPool { islsToBreak.eachParallel { Isl isl -> antiflap.portUp(isl.srcSwitch.dpId, isl.srcPort) } }
+        transitIslsToFail && withPool { transitIslsToFail.eachParallel { Isl isl -> antiflap.portUp(isl.srcSwitch.dpId, isl.srcPort) }}
+        wait(WAIT_OFFSET) {
+            assert northbound.getAllLinks().findAll { it.state == FAILED }.empty
+        }
+        northbound.deleteLinkProps(northbound.getAllLinkProps())
+        database.resetCosts()
     }
 }
