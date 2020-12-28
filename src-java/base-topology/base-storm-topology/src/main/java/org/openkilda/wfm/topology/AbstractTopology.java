d0f4dcbef20f958b95540c5974bbdf6c44bab7d6
Sergey Nikitin <snikitin@mirantis.com>
2020-12-28 12:14:31 +0400

    Restore getZkTopoName() for topologies

--- src-java/base-topology/base-storm-topology/src/main/java/org/openkilda/wfm/topology/AbstractTopology.java
+++ src-java/base-topology/base-storm-topology/src/main/java/org/openkilda/wfm/topology/AbstractTopology.java
@@ -569,4 +569,8 @@ public abstract class AbstractTopology<T extends AbstractTopologyConfig> impleme
     protected ZookeeperConfig getZookeeperConfig() {
         return zookeeperConfig;
     }
+
+    protected String getZkTopoName() {
+        return getClass().getSimpleName();
+    }
 }
