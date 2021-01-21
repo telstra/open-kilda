1e1cec0e876f87f10379f66bdbcf038e4e565dda
Andrii Dovhan <dovhan.andriy@gmail.com>
2021-01-21 12:12:21 +0200

    fix IslReplugSpec

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/links/IslReplugSpec.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/links/IslReplugSpec.groovy
@@ -140,6 +140,10 @@ class IslReplugSpec extends HealthCheckSpecification {
         then: "Original ISL becomes DISCOVERED again"
         islUtils.waitForIslStatus([isl, isl.reversed], DISCOVERED)
         def originIslIsUp = true
+        Wrappers.wait(discoveryExhaustedInterval + WAIT_OFFSET) {
+            def isls = northbound.getAllLinks()
+            [isl, isl.reversed].each { assert islUtils.getIslInfo(isls, it).get().actualState == DISCOVERED }
+        }
 
         and: "Replugged ISL status changes to MOVED"
         islUtils.waitForIslStatus([newIsl, newIsl.reversed], MOVED)
@@ -202,6 +206,10 @@ class IslReplugSpec extends HealthCheckSpecification {
         then: "Original ISL becomes DISCOVERED again"
         islUtils.waitForIslStatus([isl, isl.reversed], DISCOVERED)
         def originIslIsUp = true
+        Wrappers.wait(discoveryAuxiliaryInterval + WAIT_OFFSET) {
+            def isls = northbound.getAllLinks()
+            [isl, isl.reversed].each { assert islUtils.getIslInfo(isls, it).get().actualState == DISCOVERED }
+        }
 
         cleanup:
         if (!originIslIsUp) {
