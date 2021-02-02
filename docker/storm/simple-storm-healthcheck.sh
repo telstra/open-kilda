a5e732a4a5e23dbcb55da0506c8c1495a79b785a
Max Mazur <sirmax123@gmail.com>
2021-02-02 14:10:16 +0200

    Add check for port in logs

--- docker/storm/simple-storm-healthcheck.sh
+++ docker/storm/simple-storm-healthcheck.sh
@@ -4,14 +4,21 @@ set -eu${DEBUG:+x}
 
 set -o pipefail
 
-CONFIG=${1:-/opt/apache-storm-1.2.1/conf/storm.yaml}
+# Pre-check
+/usr/bin/jps | grep Supervisor || exit 1
 
-for PORT in $(cat ${CONFIG}  | sed -n '/supervisor.slots.ports/,/^$/p' | grep -v supervisor.slots.ports | grep -v '^$' | awk -F '-' '{ print $2}');
-do
-    echo "Checking Port ${PORT}"; 
-    netstat -ntpl | \
-        grep java | \
-        grep LISTEN | \
-        awk '{ print $4}'| \
-        grep "0.0.0.0:${PORT}" || exit 1
-done
+
+CONFIG=${1:-/opt/apache-storm-1.2.1/conf/storm.yaml}
+LOG_FILE=$(2:-/opt/apache-storm-1.2.1/logs/supervisor.log)
+
+PORTS_NUBER_IN_CONFIG=$(cat ${CONFIG}  | sed -n '/supervisor.slots.ports/,/^$/p' | grep -v supervisor.slots.ports | grep -v '^$' | awk -F '-' '{ print $2}' | wc -l )
+PORTS_NUMBER_IN_LOGS=$(head  -1000 ${LOG_FILE} | grep 'Starting in state' | wc -l )
+
+
+
+if [ ${PORTS_NUMBER_IN_LOGS} -ge ${PORTS_NUBER_IN_CONFIG} ];
+then
+    exit 2
+else
+    exit 0
+fi
