fb343583b7b6acb11f852debf8ccbd66c944dc48
Andrii Dovhan <dovhan.andriy@gmail.com>
2021-03-23 12:55:18 +0200

    add test for network/paths endpoint using LATENCY strategy

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/network/PathsSpec.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/network/PathsSpec.groovy
@@ -12,10 +12,12 @@ import org.openkilda.messaging.error.MessageError
 import org.openkilda.model.FlowEncapsulationType
 import org.openkilda.model.PathComputationStrategy
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.client.HttpClientErrorException
 
 class PathsSpec extends HealthCheckSpecification {
 
+    @Tidy
     @Tags(SMOKE)
     def "Get paths between not neighboring switches"() {
         given: "Two active not neighboring switches"
@@ -41,11 +43,41 @@ class PathsSpec extends HealthCheckSpecification {
             }
         }
 
-        and: "Cleanup: delete flow"
-        flowHelperV2.deleteFlow(flow.flowId)
+        cleanup:
+        flow && flowHelperV2.deleteFlow(flow.flowId)
     }
 
     @Tidy
+    @Tags(LOW_PRIORITY)
+    def "Able to get paths between switches for the LATENCY strategy"() {
+        given: "Two active not neighboring switches"
+        def switchPair = topologyHelper.getNotNeighboringSwitchPair()
+
+        and: "Create a flow to reduce available bandwidth on some path between these two switches"
+        def flow = flowHelperV2.addFlow(flowHelperV2.randomFlow(switchPair))
+
+        when: "Get paths between switches using the LATENCY strategy"
+        def paths = northbound.getPaths(switchPair.src.dpId, switchPair.dst.dpId, null,
+                PathComputationStrategy.LATENCY, 10, null)
+
+        then: "Paths will be sorted by latency (ascending order) and then by bandwidth (descending order)"
+        paths.paths.size() > 0
+
+        for (int i = 1; i < paths.paths.size(); i++) {
+            def prevPath = paths.paths[i - 1]
+            def curPath = paths.paths[i]
+
+            assert prevPath.latency <= curPath.latency
+
+            if (prevPath.latency == curPath.latency) { assert prevPath.bandwidth >= curPath.bandwidth }
+        }
+
+        cleanup:
+        flow && flowHelperV2.deleteFlow(flow.flowId)
+    }
+
+    @Tidy
+    @Tags(LOW_PRIORITY)
     def "Unable to get paths between one switch"() {
         given: "An active switch"
         def sw = topology.getActiveSwitches()[0]
@@ -55,10 +87,11 @@ class PathsSpec extends HealthCheckSpecification {
 
         then: "Get 400 BadRequest error because request is invalid"
         def exc = thrown(HttpClientErrorException)
-        exc.rawStatusCode == 400
+        exc.statusCode == HttpStatus.BAD_REQUEST
     }
 
     @Tidy
+    @Tags(LOW_PRIORITY)
     def "Unable to get paths between nonexistent switch"() {
         given: "An active switch"
         def sw = topology.getActiveSwitches()[0]
@@ -68,7 +101,7 @@ class PathsSpec extends HealthCheckSpecification {
 
         then: "Get 404 NotFound error"
         def exc = thrown(HttpClientErrorException)
-        exc.rawStatusCode == 404
+        exc.statusCode == HttpStatus.NOT_FOUND
     }
 
     @Tidy
@@ -80,11 +113,16 @@ class PathsSpec extends HealthCheckSpecification {
         northbound.getPaths(switchPair.src.dpId, switchPair.dst.dpId, null, PathComputationStrategy.MAX_LATENCY,
                 null, null)
 
-        then: "Get 400 BadRequest error because request is invalid"
-        def exc = thrown(HttpClientErrorException)
-        exc.rawStatusCode == 400
+        then: "Human readable error is returned"
+        def error = thrown(HttpClientErrorException)
+        error.statusCode == HttpStatus.BAD_REQUEST
+        def errorDetails = error.responseBodyAsString.to(MessageError)
+        errorDetails.errorMessage == "Missed max_latency parameter."
+        errorDetails.errorDescription == "MAX_LATENCY path computation strategy requires non null max_latency " +
+                "parameter. If max_latency will be equal to 0 LATENCY strategy will be used instead of MAX_LATENCY."
     }
 
+    @Tidy
     @Tags(LOW_PRIORITY)
     def "Unable to get a path for a 'vxlan' flowEncapsulationType when flow when switches do not support it"() {
         given: "Two active not supported 'vxlan' flowEncapsulationType switches"
@@ -102,7 +140,7 @@ class PathsSpec extends HealthCheckSpecification {
 
         then: "Human readable error is returned"
         def exc = thrown(HttpClientErrorException)
-        exc.rawStatusCode == 404
+        exc.statusCode == HttpStatus.NOT_FOUND
         // TODO(andriidovhan) fix errorMessage when the 2587 issue is fixed
         def errorDetails = exc.responseBodyAsString.to(MessageError)
         errorDetails.errorMessage == "Switch $switchPair.src.dpId doesn't have links with enough bandwidth"
