625ba4849af61bf045c2c3bbfdbbb6fae0157f7a
rtretyak <acedia.ua@gmail.com>
2021-10-04 23:10:38 +0300

    tests: fix hanging after context recreation

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/listeners/ReleaseLabListener.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/listeners/ReleaseLabListener.groovy
@@ -2,7 +2,6 @@ package org.openkilda.functionaltests.listeners
 
 import org.openkilda.functionaltests.BaseSpecification
 import org.openkilda.testing.model.topology.TopologyDefinition
-import org.openkilda.testing.tools.TopologyPool
 
 import org.spockframework.runtime.model.SpecInfo
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,14 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired
 class ReleaseLabListener extends AbstractSpringListener {
     @Autowired
     TopologyDefinition topology
-    @Autowired
-    TopologyPool topologyPool
 
     void afterSpec(SpecInfo runningSpec) {
         if (context) {
             context.autowireCapableBeanFactory.autowireBean(this)
             BaseSpecification.threadLocalTopology.set(null)
-            topologyPool.put(topology)
+            topology.returnToPool()
         }
     }
 }
