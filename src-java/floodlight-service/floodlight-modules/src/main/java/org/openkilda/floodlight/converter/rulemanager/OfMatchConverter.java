00eb716850d77dacb25ab031104884b091ba8bcd
Timofey Durakov <timofei.nd@gmail.com>
2021-12-28 13:52:08 +0400

    Refactor OfMatchConverter

--- src-java/floodlight-service/floodlight-modules/src/main/java/org/openkilda/floodlight/converter/rulemanager/OfMatchConverter.java
+++ src-java/floodlight-service/floodlight-modules/src/main/java/org/openkilda/floodlight/converter/rulemanager/OfMatchConverter.java
@@ -19,7 +19,9 @@ import static java.lang.String.format;
 
 import org.openkilda.rulemanager.Field;
 import org.openkilda.rulemanager.match.FieldMatch;
+import org.openkilda.rulemanager.match.FieldMatch.FieldMatchBuilder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import org.projectfloodlight.openflow.protocol.OFFactory;
@@ -32,13 +34,17 @@ import org.projectfloodlight.openflow.types.MacAddress;
 import org.projectfloodlight.openflow.types.Masked;
 import org.projectfloodlight.openflow.types.OFMetadata;
 import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.OFValueType;
+import org.projectfloodlight.openflow.types.OFVlanVidMatch;
 import org.projectfloodlight.openflow.types.TransportPort;
+import org.projectfloodlight.openflow.types.U32;
 import org.projectfloodlight.openflow.types.U64;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Mapper
+@Slf4j
 public class OfMatchConverter {
     public static final OfMatchConverter INSTANCE = Mappers.getMapper(OfMatchConverter.class);
 
@@ -46,135 +52,150 @@ public class OfMatchConverter {
     /**
      * Convert match.
      */
-    // TODO(tdurakov): reimplement with match.getMatchFields()
     public Set<FieldMatch> convertToRuleManagerMatch(Match match) {
         Set<FieldMatch> fieldMatches = new HashSet<>();
-        // ETH_SRC
-        Masked<MacAddress> ethSrcMasked = match.getMasked(MatchField.ETH_SRC);
-        if (ethSrcMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.ETH_SRC)
-                    .value(ethSrcMasked.getValue().getLong())
-                    .mask(ethSrcMasked.getMask().getLong())
-                    .build());
-        } else {
-            MacAddress ethSrc = match.get(MatchField.ETH_SRC);
-            if (ethSrc != null) {
-                fieldMatches.add(FieldMatch.builder().field(Field.ETH_SRC).value(ethSrc.getLong()).build());
-            }
-        }
-
-        // ETH_DST
-        Masked<MacAddress> ethDstMasked = match.getMasked(MatchField.ETH_DST);
-        if (ethDstMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.ETH_DST)
-                    .value(ethDstMasked.getValue().getLong()).mask(ethDstMasked.getMask().getLong())
-                    .build());
-        } else {
-            MacAddress ethDst = match.get(MatchField.ETH_DST);
-            if (ethDst != null) {
-                fieldMatches.add(FieldMatch.builder().field(Field.ETH_DST).value(ethDst.getLong()).build());
-            }
-        }
-
-        // ETH_TYPE
-        Masked<EthType> ethTypeMasked = match.getMasked(MatchField.ETH_TYPE);
-        if (ethTypeMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.ETH_TYPE)
-                    .value(ethTypeMasked.getValue().getValue())
-                    .mask((long) ethTypeMasked.getMask().getValue())
-                    .build());
-        } else {
-            EthType ethType = match.get(MatchField.ETH_TYPE);
-            if (ethType != null) {
-                fieldMatches.add(FieldMatch.builder().field(Field.ETH_TYPE).value(ethType.getValue()).build());
-            }
-        }
-
-        // IP_PROTO
-        Masked<IpProtocol> ipProtocolMasked = match.getMasked(MatchField.IP_PROTO);
-        if (ethTypeMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.IP_PROTO)
-                    .value(ipProtocolMasked.getValue().getIpProtocolNumber())
-                    .mask((long) ipProtocolMasked.getMask().getIpProtocolNumber())
-                    .build());
-        } else {
-            IpProtocol ipProtocol = match.get(MatchField.IP_PROTO);
-            if (ipProtocol != null) {
-                fieldMatches.add(
-                        FieldMatch.builder().field(Field.IP_PROTO).value(ipProtocol.getIpProtocolNumber()).build());
-            }
-        }
-
-        // UDP_SRC
-        Masked<TransportPort> udpSrcMasked = match.getMasked(MatchField.UDP_SRC);
-        if (udpSrcMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.UDP_SRC)
-                    .value(udpSrcMasked.getValue().getPort())
-                    .mask((long) udpSrcMasked.getMask().getPort())
-                    .build());
-        } else {
-            TransportPort udpSrc = match.get(MatchField.UDP_SRC);
-            if (udpSrc != null) {
-                fieldMatches.add(FieldMatch.builder().field(Field.UDP_SRC).value(udpSrc.getPort()).build());
-            }
-        }
-
-        // UDP_DST
-        Masked<TransportPort> udpDstMasked = match.getMasked(MatchField.UDP_DST);
-        if (udpDstMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.UDP_DST)
-                    .value(udpDstMasked.getValue().getPort())
-                    .mask((long) udpDstMasked.getMask().getPort())
-                    .build());
-        } else {
-            TransportPort udpDst = match.get(MatchField.UDP_DST);
-            if (udpDst != null) {
-                fieldMatches.add(FieldMatch.builder().field(Field.UDP_DST).value(udpDst.getPort()).build());
-            }
-        }
-
-        // METADATA
-        Masked<OFMetadata> metadataMasked = match.getMasked(MatchField.METADATA);
-        if (metadataMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.METADATA)
-                    .value(metadataMasked.getValue().getValue().getValue())
-                    .mask((long) metadataMasked.getMask().getValue().getValue())
-                    .build());
-        } else {
-            OFMetadata metadata = match.get(MatchField.METADATA);
-            if (metadata != null) {
-                fieldMatches.add(FieldMatch.builder()
-                        .field(Field.METADATA)
-                        .value(metadata.getValue().getValue())
-                        .build());
-            }
-        }
-        Masked<OFPort> inPortMasked = match.getMasked(MatchField.IN_PORT);
-        if (inPortMasked != null) {
-            fieldMatches.add(FieldMatch.builder()
-                    .field(Field.IN_PORT)
-                    .value(inPortMasked.getValue().getPortNumber())
-                    .mask((long) inPortMasked.getMask().getPortNumber())
-                    .build());
-        } else {
-            OFPort inPort = match.get(MatchField.IN_PORT);
-            if (inPort != null) {
-                fieldMatches.add(FieldMatch.builder()
-                        .field(Field.IN_PORT)
-                        .value(inPort.getPortNumber())
-                        .build());
+        for (MatchField field : match.getMatchFields()) {
+            if (match.isExact(field)) {
+                fieldMatches.add(getExact(match, field));
+            } else {
+                fieldMatches.add(getMasked(match, field));
             }
         }
         return fieldMatches;
     }
 
+    private FieldMatch getExact(Match match, MatchField field) {
+        OFValueType ofValueType = match.get(field);
+
+        FieldMatchBuilder builder = FieldMatch.builder();
+        switch (field.id) {
+            case ETH_SRC:
+                builder.field(Field.ETH_SRC);
+                builder.value(match.get(MatchField.ETH_SRC).getLong());
+                break;
+            case ETH_DST:
+                builder.field(Field.ETH_DST);
+                builder.value(match.get(MatchField.ETH_DST).getLong());
+                break;
+            case ETH_TYPE:
+                builder.field(Field.ETH_TYPE);
+                builder.value(match.get(MatchField.ETH_TYPE).getValue());
+                break;
+            case IP_PROTO:
+                builder.field(Field.IP_PROTO);
+                builder.value(match.get(MatchField.IP_PROTO).getIpProtocolNumber());
+                break;
+            case UDP_SRC:
+                builder.field(Field.UDP_SRC);
+                builder.value(match.get(MatchField.UDP_SRC).getPort());
+                break;
+            case UDP_DST:
+                builder.field(Field.UDP_DST);
+                builder.value(match.get(MatchField.UDP_DST).getPort());
+                break;
+            case METADATA:
+                builder.field(Field.METADATA);
+                builder.value(match.get(MatchField.METADATA).getValue().getValue());
+                break;
+            case IN_PORT:
+                builder.field(Field.IN_PORT);
+                builder.value(match.get(MatchField.IN_PORT).getPortNumber());
+                break;
+            case VLAN_VID:
+                builder.field(Field.VLAN_VID);
+                builder.value(match.get(MatchField.VLAN_VID).getVlan());
+                break;
+            case TUNNEL_ID:
+                builder.field(Field.NOVIFLOW_TUNNEL_ID);
+                builder.value(match.get(MatchField.TUNNEL_ID).getValue());
+                break;
+            case KILDA_VXLAN_VNI:
+                builder.field(Field.OVS_VXLAN_VNI);
+                builder.value(match.get(MatchField.KILDA_VXLAN_VNI).getValue());
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        String.format("Unexpected match field id=%s, for class=%s of a match", field.id,
+                                field.getClass().getName(), match));
+        }
+        return builder.build();
+    }
+
+    private FieldMatch getMasked(Match match, MatchField field) {
+        FieldMatchBuilder builder = FieldMatch.builder();
+        switch (field.id) {
+            case ETH_SRC:
+                Masked<MacAddress> ethSrcMasked = match.getMasked(MatchField.ETH_SRC);
+                builder.field(Field.ETH_SRC);
+                builder.value(ethSrcMasked.getValue().getLong());
+                builder.mask(ethSrcMasked.getMask().getLong());
+                break;
+            case ETH_DST:
+                Masked<MacAddress> ethDstMasked = match.getMasked(MatchField.ETH_DST);
+                builder.field(Field.ETH_DST);
+                builder.value(ethDstMasked.getValue().getLong());
+                builder.mask(ethDstMasked.getMask().getLong());
+                break;
+            case ETH_TYPE:
+                Masked<EthType> ethTypeMasked = match.getMasked(MatchField.ETH_TYPE);
+                builder.field(Field.ETH_TYPE);
+                builder.value(ethTypeMasked.getValue().getValue());
+                builder.mask((long) ethTypeMasked.getMask().getValue());
+                break;
+            case IP_PROTO:
+                Masked<IpProtocol> ipProtocolMasked = match.getMasked(MatchField.IP_PROTO);
+                builder.field(Field.IP_PROTO);
+                builder.value(ipProtocolMasked.getValue().getIpProtocolNumber());
+                builder.mask((long) ipProtocolMasked.getMask().getIpProtocolNumber());
+                break;
+            case UDP_SRC:
+                Masked<TransportPort> udpSrcMasked = match.getMasked(MatchField.UDP_SRC);
+                builder.field(Field.UDP_SRC);
+                builder.value(udpSrcMasked.getValue().getPort());
+                builder.mask((long) udpSrcMasked.getMask().getPort());
+                break;
+            case UDP_DST:
+                Masked<TransportPort> udpDstMasked = match.getMasked(MatchField.UDP_DST);
+                builder.field(Field.UDP_DST);
+                builder.value(udpDstMasked.getValue().getPort());
+                builder.mask((long) udpDstMasked.getMask().getPort());
+                break;
+            case METADATA:
+                Masked<OFMetadata> metadataMasked = match.getMasked(MatchField.METADATA);
+                builder.field(Field.METADATA);
+                builder.value(metadataMasked.getValue().getValue().getValue());
+                builder.mask(metadataMasked.getMask().getValue().getValue());
+                break;
+            case IN_PORT:
+                Masked<OFPort> inPortMasked = match.getMasked(MatchField.IN_PORT);
+                builder.field(Field.IN_PORT);
+                builder.value(inPortMasked.getValue().getPortNumber());
+                builder.mask((long) inPortMasked.getMask().getPortNumber());
+                break;
+            case VLAN_VID:
+                Masked<OFVlanVidMatch> vlanVidMasked = match.getMasked(MatchField.VLAN_VID);
+                builder.field(Field.VLAN_VID);
+                builder.value(vlanVidMasked.getValue().getVlan());
+                builder.mask((long) vlanVidMasked.getMask().getVlan());
+                break;
+            case TUNNEL_ID:
+                Masked<U64> tunnelIdMasked = match.getMasked(MatchField.TUNNEL_ID);
+                builder.field(Field.NOVIFLOW_TUNNEL_ID);
+                builder.value(tunnelIdMasked.getValue().getValue());
+                builder.mask(tunnelIdMasked.getMask().getValue());
+                break;
+            case KILDA_VXLAN_VNI:
+                Masked<U32> masked = match.getMasked(MatchField.KILDA_VXLAN_VNI);
+                builder.field(Field.OVS_VXLAN_VNI);
+                builder.value(masked.getValue().getValue());
+                builder.mask(masked.getMask().getValue());
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        String.format("Unexpected match field id=%s, for class=%s of a match", field.id,
+                                field.getClass().getName(), match));        }
+        return builder.build();
+    }
 
     /**
      * Convert match.
@@ -251,7 +272,30 @@ public class OfMatchConverter {
                     builder.setExact(MatchField.METADATA, OFMetadata.of(U64.of(fieldMatch.getValue())));
                 }
                 break;
-            //todo (rule-manager-fl-integration): add other fields
+            case VLAN_VID:
+                if (fieldMatch.isMasked()) {
+                    builder.setMasked(MatchField.VLAN_VID, OFVlanVidMatch.ofVlan((int) fieldMatch.getValue()),
+                            OFVlanVidMatch.ofVlan(fieldMatch.getMask().intValue()));
+                } else {
+                    builder.setExact(MatchField.VLAN_VID, OFVlanVidMatch.ofVlan((int) fieldMatch.getValue()));
+                }
+                break;
+            case OVS_VXLAN_VNI:
+                if (fieldMatch.isMasked()) {
+                    builder.setMasked(MatchField.KILDA_VXLAN_VNI, U32.of(fieldMatch.getValue()),
+                            U32.of(fieldMatch.getMask()));
+                } else {
+                    builder.setExact(MatchField.KILDA_VXLAN_VNI, U32.of(fieldMatch.getValue()));
+                }
+                break;
+            case NOVIFLOW_TUNNEL_ID:
+                if (fieldMatch.isMasked()) {
+                    builder.setMasked(MatchField.TUNNEL_ID, U64.of(fieldMatch.getValue()),
+                            U64.of(fieldMatch.getMask()));
+                } else {
+                    builder.setExact(MatchField.TUNNEL_ID, U64.of(fieldMatch.getValue()));
+                }
+                break;
             default:
                 throw new IllegalStateException(format("Unknown field match %s", fieldMatch.getField()));
         }
