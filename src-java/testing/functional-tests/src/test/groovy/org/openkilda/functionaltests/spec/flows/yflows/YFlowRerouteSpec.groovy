5b651dea60f4d9532fb56fd2f1a75ec7f575f6fd
Andrii Dovhan <dovhan.andriy@gmail.com>
2022-03-14 13:17:22 +0200

    [test] improve YFlowRerouteSpec

--- src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/yflows/YFlowRerouteSpec.groovy
+++ src-java/testing/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/yflows/YFlowRerouteSpec.groovy
@@ -15,12 +15,16 @@ import org.openkilda.functionaltests.extension.failfast.Tidy
 import org.openkilda.functionaltests.extension.tags.Tags
 import org.openkilda.functionaltests.helpers.PathHelper
 import org.openkilda.functionaltests.helpers.YFlowHelper
+import org.openkilda.functionaltests.helpers.model.SwitchTriplet
+import org.openkilda.messaging.info.event.PathNode
 import org.openkilda.messaging.payload.flow.FlowState
+import org.openkilda.testing.model.topology.TopologyDefinition.Switch
 import org.openkilda.testing.service.traffexam.TraffExamService
 import org.openkilda.testing.service.traffexam.model.Exam
 import org.openkilda.testing.service.traffexam.model.ExamReport
 import org.openkilda.testing.tools.FlowTrafficExamBuilder
 
+import groovy.transform.Memoized
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Narrative
@@ -43,10 +47,12 @@ class YFlowRerouteSpec extends HealthCheckSpecification {
     def "Valid y-flow can be rerouted"() {
         assumeTrue(useMultitable, "Multi table is not enabled in kilda configuration")
         given: "A qinq y-flow"
-        def swT = topologyHelper.switchTriplets.find {
+        def swT = topologyHelper.switchTriplets.find { it ->
+            def yPoints = findPotentialYPoints(it)
             [it.shared, it.ep1, it.ep2].every { it.traffGens } &&
                     [it.pathsEp1, it.pathsEp2].every { it.size() > 1 } &&
-                    it.ep1 != it.ep2
+                    it.ep1 != it.ep2 && yPoints.size() == 1 && yPoints[0] != it.shared &&
+                    !it.shared.wb5164 && !it.ep1.wb5164 && !it.ep2.wb5164
         }
         assumeTrue(swT != null, "These cases cannot be covered on given topology:")
         def yFlowRequest = yFlowHelper.randomYFlow(swT).tap {
@@ -125,5 +131,25 @@ class YFlowRerouteSpec extends HealthCheckSpecification {
         islToFail && antiflap.portUp(islToFail.srcSwitch.dpId, islToFail.srcPort)
         wait(WAIT_OFFSET) { northbound.getLink(islToFail).state == DISCOVERED }
         database.resetCosts(topology.isls)
+    }
+
+    @Memoized
+    List<Switch> findPotentialYPoints(SwitchTriplet swT) {
+        def sortedEp1Paths = swT.pathsEp1.sort { it.size() }
+        def potentialEp1Paths = sortedEp1Paths.takeWhile { it.size() == sortedEp1Paths[0].size() }
+        def potentialEp2Paths = potentialEp1Paths.collect { potentialEp1Path ->
+            def sortedEp2Paths = swT.pathsEp2.sort {
+                it.size() - it.intersect(potentialEp1Path).size()
+            }
+            [path1: potentialEp1Path,
+             potentialPaths2: sortedEp2Paths.takeWhile {it.size() == sortedEp2Paths[0].size() }]
+        }
+        return potentialEp2Paths.collectMany {path1WithPath2 ->
+            path1WithPath2.potentialPaths2.collect { List<PathNode> potentialPath2 ->
+                def switches = pathHelper.getInvolvedSwitches(path1WithPath2.path1)
+                        .intersect(pathHelper.getInvolvedSwitches(potentialPath2))
+                switches ? switches[-1] : null
+            }
+        }.findAll().unique()
     }
 }
