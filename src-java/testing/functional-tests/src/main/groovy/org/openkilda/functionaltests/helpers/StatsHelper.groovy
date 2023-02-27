881727f4ec4d02898bfab671b1ec6dd032909df0
Sergey Nikitin <snikitin@mirantis.com>
2023-02-27 15:23:52 +0100

    Revert "Revert "#5017: #4926: Add tests to verify missing YFlow/subflows stats""
    This reverts commit 0480d0f51d000fd37313893a4a4c0d260bbc506b.

--- src-java/testing/functional-tests/src/main/groovy/org/openkilda/functionaltests/helpers/StatsHelper.groovy
+++ src-java/testing/functional-tests/src/main/groovy/org/openkilda/functionaltests/helpers/StatsHelper.groovy
@@ -1,15 +1,20 @@
 package org.openkilda.functionaltests.helpers
 
+import org.openkilda.functionaltests.helpers.Wrappers.WaitTimeoutException
+import org.openkilda.functionaltests.model.stats.StatsQueryFilter
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.openkilda.messaging.command.CommandMessage
 import org.openkilda.messaging.command.stats.StatsRequest
 import org.openkilda.northbound.dto.v2.yflows.YFlow
 import org.openkilda.testing.service.otsdb.OtsdbQueryService
+import org.openkilda.testing.service.otsdb.model.StatsResult
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+
+import static groovyx.gpars.GParsPool.withPool
 
 import static org.openkilda.testing.Constants.DefaultRule
 import static org.openkilda.testing.Constants.STATS_LOGGING_TIMEOUT
@@ -28,6 +33,8 @@ class StatsHelper {
     String metricPrefix
     final String KAFKA_STORM_SPEAKER_TOPIC = "kilda.speaker.storm"
 
+    KafkaProducer kafkaProducer = null
+
     void verifyFlowWritesStats(String flowId, Date from, boolean expectTraffic) {
         "force kilda to collect stats"(flowId)
         Wrappers.wait(STATS_LOGGING_TIMEOUT) {
@@ -40,7 +47,7 @@ class StatsHelper {
         }
     }
 
-    void verifyYFlowWritesMeterStats(YFlow yFlow, Date from, boolean expectTraffic) {
+    void verifyYFlowWritesStats(YFlow yFlow, Date from, boolean expectTraffic) {
         "force kilda to collect stats"(yFlow.getYFlowId())
         Wrappers.wait(STATS_LOGGING_TIMEOUT) {
             def yFlowId = yFlow.YFlowId
@@ -61,7 +68,7 @@ class StatsHelper {
         }
     }
 
-    long 'ping packets count on switch'(Date beforePingTime, Switch srcSwitch, DefaultRule rule) {
+    long "ping packets count on switch"(Date beforePingTime, Switch srcSwitch, DefaultRule rule) {
         def statsData = null
         "force kilda to collect stats"()
         Wrappers.wait(STATS_LOGGING_TIMEOUT, 2) {
@@ -73,12 +80,46 @@ class StatsHelper {
         return statsData.values().last().toLong()
     }
 
-    private void "force kilda to collect stats"(String flowId = "generic") {
-        KafkaProducer kafkaProducer = new KafkaProducer(producerProps)
-        kafkaProducer.send(new ProducerRecord(KAFKA_STORM_SPEAKER_TOPIC,
+    List<StatsResult> "get multiple stats results"(Date from, List<StatsQueryFilter> statsToRetrieve) {
+        List<StatsResult> results = null
+        "force kilda to collect stats"()
+        try {
+            Wrappers.wait(STATS_LOGGING_TIMEOUT, 2) {
+                withPool {
+                    results = statsToRetrieve.collectParallel {
+                        "get stats from DB"(from, it)
+                    }
+                }
+                assert results.every {it.getDps().values().any { it > 0 }
+                }
+            }
+        } catch (WaitTimeoutException e) {
+            //don't do anything, we will verify stats correctness in StatsResult's class methods
+        }
+        return results
+    }
+
+    StatsResult "get single stats result"(Date from, StatsQueryFilter statsToRetrieve) {
+        return "get multiple stats results"(from, [statsToRetrieve])[0]
+    }
+
+    void "force kilda to collect stats"(String flowId = "generic") {
+        getKafkaProducer().send(new ProducerRecord(KAFKA_STORM_SPEAKER_TOPIC,
                 new CommandMessage(
                         new StatsRequest(),
                         System.currentTimeMillis(),
                         "artificial autotest stats collection enforcement for flow ${flowId}").toJson())).get()
+    }
+
+    private StatsResult "get stats from DB"(Date from, StatsQueryFilter filter) {
+        return otsdb.query(from, "${metricPrefix}${filter.getMetric().getMetric()}", filter.getTags())
+    }
+
+    //Something like singleton here. If you know the better approach, please, suggest in comments or refactor
+    private KafkaProducer getKafkaProducer() {
+        if (this.kafkaProducer == null) {
+            this.kafkaProducer = new KafkaProducer(producerProps)
+        }
+        return this.kafkaProducer
     }
 }
