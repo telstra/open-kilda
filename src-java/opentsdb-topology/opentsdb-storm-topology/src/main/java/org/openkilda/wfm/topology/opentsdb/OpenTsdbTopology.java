eabda5ed5ed2eae680d6df030e2f1970a7b2c475
Sergey Nikitin <snikitin@mirantis.com>
2023-08-09 01:44:12 +0200

    Restore old Otsdb Config

--- src-java/opentsdb-topology/opentsdb-storm-topology/src/main/java/org/openkilda/wfm/topology/opentsdb/OpenTsdbTopology.java
+++ src-java/opentsdb-topology/opentsdb-storm-topology/src/main/java/org/openkilda/wfm/topology/opentsdb/OpenTsdbTopology.java
@@ -31,7 +31,6 @@ import com.google.common.annotations.VisibleForTesting;
 import org.apache.storm.generated.StormTopology;
 import org.apache.storm.kafka.spout.KafkaSpout;
 import org.apache.storm.kafka.spout.KafkaSpoutConfig;
-import org.apache.storm.kafka.spout.KafkaSpoutConfig.FirstPollOffsetStrategy;
 import org.apache.storm.opentsdb.bolt.OpenTsdbBolt;
 import org.apache.storm.opentsdb.client.OpenTsdbClient;
 import org.apache.storm.topology.BoltDeclarer;
@@ -120,12 +119,13 @@ public class OpenTsdbTopology extends AbstractTopology<OpenTsdbTopologyConfig> {
         String topic = topologyConfig.getKafkaTopics().getOtsdbTopic();
 
         //FIXME: We have to use the Message class for messaging (but current setup saves some space/traffic in stream).
-        KafkaSpoutConfig<String, InfoData> config = makeKafkaSpoutConfig(
-                Collections.singletonList(topic), spoutId, InfoDataDeserializer.class)
+        KafkaSpoutConfig<String, InfoData> config = getKafkaSpoutConfigBuilder(topic, OTSDB_SPOUT_ID)
+                .setValue(InfoDataDeserializer.class)
                 .setRecordTranslator(new InfoDataTranslator())
-                .setFirstPollOffsetStrategy(FirstPollOffsetStrategy.UNCOMMITTED_EARLIEST)
+                .setFirstPollOffsetStrategy(KafkaSpoutConfig.FirstPollOffsetStrategy.UNCOMMITTED_EARLIEST)
                 .setTupleTrackingEnforced(true)
                 .build();
+
 
         KafkaSpout<String, InfoData> kafkaSpout = new KafkaSpout<>(config);
         declareSpout(topology, kafkaSpout, spoutId);
