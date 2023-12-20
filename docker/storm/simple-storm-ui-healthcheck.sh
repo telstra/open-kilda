f1550ecb5c6d1763fe117db3b9a73f5c1981d084
ichupin <ivan.chupin.95@gmail.com>
2023-12-20 14:29:57 +0000

    storm-nimbus healthcheck fix sotrm version fix

--- docker/storm/simple-storm-ui-healthcheck.sh
+++ docker/storm/simple-storm-ui-healthcheck.sh
@@ -8,7 +8,7 @@ set -o pipefail
 /usr/bin/jps | grep --silent core || exit 1
 
 
-CONFIG=${1:-/opt/apache-storm-1.2.1/conf/storm.yaml}
+CONFIG=${1:-/opt/apache-storm-2.5.0/conf/storm.yaml}
 
 PORTS_NUBER_IN_CONFIG=$(cat ${CONFIG}  | sed -n '/supervisor.slots.ports/,/^$/p' | grep -v supervisor.slots.ports | grep -v '^$' | awk -F '-' '{ print $2}' | wc -l )
 ACTUAL_SLOTS=$(curl  -q 127.0.0.1:8080/api/v1/cluster/summary 2>/dev/null | jq -r  '.slotsTotal')
