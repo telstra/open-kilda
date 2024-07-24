c2dc57379a52cfc97b1eded2165a57258c7267cb
oleksir <oleksii.sirobaba@mirantis.com>
2024-07-24 11:51:58 +0200

    Add API to flush reroute for pending flow (fix response)
    Signed-off-by: olesir <osirobaba@mirantis.come>

--- src-java/northbound-service/northbound/src/main/java/org/openkilda/northbound/controller/v1/FlowController.java
+++ src-java/northbound-service/northbound/src/main/java/org/openkilda/northbound/controller/v1/FlowController.java
@@ -20,6 +20,7 @@ import static java.lang.String.format;
 import org.openkilda.messaging.error.ErrorType;
 import org.openkilda.messaging.error.MessageException;
 import org.openkilda.messaging.info.meter.FlowMeterEntries;
+import org.openkilda.messaging.info.reroute.FlowType;
 import org.openkilda.messaging.payload.flow.FlowCreatePayload;
 import org.openkilda.messaging.payload.flow.FlowFlushReroutePayload;
 import org.openkilda.messaging.payload.flow.FlowIdStatusPayload;
@@ -251,11 +252,13 @@ public class FlowController extends BaseController {
      * @param flowId id of flow to be flushed.
      * @return flow payload with updated path.
      */
-    @ApiOperation(value = "Reroute flow", response = FlowFlushReroutePayload.class)
+    @ApiOperation(value = "Reroute flow of specified type", response = FlowFlushReroutePayload.class)
     @PatchMapping(path = "/{flow_id}/reroute/flush")
     @ResponseStatus(HttpStatus.OK)
-    public CompletableFuture<FlowFlushReroutePayload> flushRerouteFlow(@PathVariable("flow_id") String flowId) {
-        return flowService.flushRerouteFlow(flowId);
+    public CompletableFuture<FlowFlushReroutePayload> flushRerouteFlow(
+            @PathVariable("flow_id") String flowId,
+            @RequestParam(value = "flow_type", defaultValue = "FLOW") FlowType flowType) {
+        return flowService.flushRerouteFlow(flowId, flowType);
     }
 
     /**
